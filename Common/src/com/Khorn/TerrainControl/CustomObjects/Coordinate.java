608940f3964d44369ebed4cb344a3b133fa29e70
Wickth <wick.000@gmail.com>
2012-02-18 05:11:45 +0400

    Fix block rotate in custom objects.

--- Common/src/com/Khorn/TerrainControl/CustomObjects/Coordinate.java
+++ Common/src/com/Khorn/TerrainControl/CustomObjects/Coordinate.java
@@ -1,5 +1,7 @@
 package com.Khorn.TerrainControl.CustomObjects;
 
+import com.Khorn.TerrainControl.DefaultMaterial;
+
 public class Coordinate
 {
 
@@ -41,196 +43,152 @@ public class Coordinate
     @SuppressWarnings("PointlessBitwiseExpression")
     public static int RotateData(int type, int data)
     {
+        DefaultMaterial mat = DefaultMaterial.getMaterial(type);
+        if(mat == null)
+            return data;
 
-        switch (type)
-        {
-            case 50:
-            case 75:
-            case 76:
-                switch (data)
-                {
-                    case 1:
-                        return 3;
-                    case 2:
-                        return 4;
-                    case 3:
-                        return 2;
-                    case 4:
-                        return 1;
+        switch (mat) {
+            case TORCH:
+            case REDSTONE_TORCH_OFF:
+            case REDSTONE_TORCH_ON:
+                switch (data) {
+                    case 3: return 1;
+                    case 4: return 2;
+                    case 2: return 3;
+                    case 1: return 4;
                 }
                 break;
-            case 66:
-                switch (data)
-                {
-                    case 6:
-                        return 7;
-                    case 7:
-                        return 8;
-                    case 8:
-                        return 9;
-                    case 9:
-                        return 6;
+
+            case RAILS:
+                switch (data) {
+                    case 7: return 6;
+                    case 8: return 7;
+                    case 9: return 8;
+                    case 6: return 9;
                 }
 
-            case 27:
-            case 28:
-                switch (data & 0x7)
-                {
-                    case 0:
-                        return 0x1 | data & 0xFFFFFFF8;
-                    case 1:
-                        return 0x0 | data & 0xFFFFFFF8;
-                    case 2:
-                        return 0x5 | data & 0xFFFFFFF8;
-                    case 3:
-                        return 0x4 | data & 0xFFFFFFF8;
-                    case 4:
-                        return 0x2 | data & 0xFFFFFFF8;
-                    case 5:
-                        return 0x3 | data & 0xFFFFFFF8;
+            case POWERED_RAIL:
+            case DETECTOR_RAIL:
+                int power = data & ~0x7;
+                switch (data & 0x7) {
+                    case 1: return 0 | power;
+                    case 0: return 1 | power;
+                    case 5: return 2 | power;
+                    case 4: return 3 | power;
+                    case 2: return 4 | power;
+                    case 3: return 5 | power;
                 }
                 break;
-            case 53:
-            case 67:
-            case 108:
-            case 109:
-            case 114:
-                switch (data)
-                {
-                    case 0:
-                        return 2;
-                    case 1:
-                        return 3;
-                    case 2:
-                        return 1;
-                    case 3:
-                        return 0;
+
+            case WOOD_STAIRS:
+            case COBBLESTONE_STAIRS:
+            case BRICK_STAIRS:
+            case SMOOTH_STAIRS:
+            case NETHER_BRICK_STAIRS:
+                switch (data) {
+                    case 2: return 0;
+                    case 3: return 1;
+                    case 1: return 2;
+                    case 0: return 3;
                 }
                 break;
-            case 69:
-            case 77:
+
+            case LEVER:
+            case STONE_BUTTON:
                 int thrown = data & 0x8;
-                int withoutThrown = data & 0xFFFFFFF7;
-                switch (withoutThrown)
-                {
-                    case 1:
-                        return 0x3 | thrown;
-                    case 2:
-                        return 0x4 | thrown;
-                    case 3:
-                        return 0x2 | thrown;
-                    case 4:
-                        return 0x1 | thrown;
+                int withoutThrown = data & ~0x8;
+                switch (withoutThrown) {
+                    case 3: return 1 | thrown;
+                    case 4: return 2 | thrown;
+                    case 2: return 3 | thrown;
+                    case 1: return 4 | thrown;
                 }
                 break;
-            case 64:
-            case 71:
+
+            case WOODEN_DOOR:
+            case IRON_DOOR_BLOCK:
                 int topHalf = data & 0x8;
                 int swung = data & 0x4;
-                int withoutFlags = data & 0xFFFFFFF3;
-                switch (withoutFlags)
-                {
-                    case 0:
-                        return 0x1 | topHalf | swung;
-                    case 1:
-                        return 0x2 | topHalf | swung;
-                    case 2:
-                        return 0x3 | topHalf | swung;
-                    case 3:
-                        return 0x0 | topHalf | swung;
+                int withoutFlags = data & ~(0x8 | 0x4);
+                switch (withoutFlags) {
+                    case 1: return 0 | topHalf | swung;
+                    case 2: return 1 | topHalf | swung;
+                    case 3: return 2 | topHalf | swung;
+                    case 0: return 3 | topHalf | swung;
                 }
                 break;
-            case 63:
-                return (data + 4) % 16;
-            case 23:
-            case 54:
-            case 61:
-            case 62:
-            case 65:
-            case 68:
-                switch (data)
-                {
-                    case 2:
-                        return 5;
-                    case 3:
-                        return 4;
-                    case 4:
-                        return 2;
-                    case 5:
-                        return 3;
-                }
-                break;
-            case 86:
-            case 91:
-                switch (data)
-                {
-                    case 0:
-                        return 1;
-                    case 1:
-                        return 2;
-                    case 2:
-                        return 3;
-                    case 3:
-                        return 0;
-                }
-                break;
-            case 93:
-            case 94:
-                int dir = data & 0x3;
-                int delay = data - dir;
-                switch (dir)
-                {
-                    case 0:
-                        return 0x1 | delay;
-                    case 1:
-                        return 0x2 | delay;
-                    case 2:
-                        return 0x3 | delay;
-                    case 3:
-                        return 0x0 | delay;
-                }
-                break;
-            case 96:
-                int withoutOrientation = data & 0xFFFFFFFC;
-                int orientation = data & 0x3;
-                switch (orientation)
-                {
-                    case 0:
-                        return 0x3 | withoutOrientation;
-                    case 1:
-                        return 0x2 | withoutOrientation;
-                    case 2:
-                        return 0x0 | withoutOrientation;
-                    case 3:
-                        return 0x1 | withoutOrientation;
-                }
-                break;
-            case 29:
-            case 33:
-            case 34:
-                int rest = data & 0xFFFFFFF8;
-                switch (data & 0x7)
-                {
-                    case 2:
-                        return 0x5 | rest;
-                    case 3:
-                        return 0x4 | rest;
-                    case 4:
-                        return 0x2 | rest;
-                    case 5:
-                        return 0x3 | rest;
-                }
-                break;
-            case 99:
-            case 100:
-                if (data >= 10)
-                    return data;
-                return data * 3 % 10;
-            case 106:
-                return (data << 1 | data >> 3) & 0xF;
-            case 107:
-                return data + 1 & 0x3 | data & 0xFFFFFFFC;
 
+            case SIGN_POST:
+                return (data + 12) % 16;
+
+            case LADDER:
+            case WALL_SIGN:
+            case CHEST:
+            case FURNACE:
+            case BURNING_FURNACE:
+            case DISPENSER:
+                switch (data) {
+                    case 5: return 2;
+                    case 4: return 3;
+                    case 2: return 4;
+                    case 3: return 5;
+                }
+                break;
+
+            case PUMPKIN:
+            case JACK_O_LANTERN:
+                switch (data) {
+                    case 1: return 0;
+                    case 2: return 1;
+                    case 3: return 2;
+                    case 0: return 3;
+                }
+                break;
+
+            case DIODE_BLOCK_OFF:
+            case DIODE_BLOCK_ON:
+                int dir = data & 0x03;
+                int delay = data - dir;
+                switch (dir) {
+                    case 1: return 0 | delay;
+                    case 2: return 1 | delay;
+                    case 3: return 2 | delay;
+                    case 0: return 3 | delay;
+                }
+                break;
+
+            case TRAP_DOOR:
+                int withoutOrientation = data & ~0x3;
+                int orientation = data & 0x3;
+                switch (orientation) {
+                    case 3: return 0 | withoutOrientation;
+                    case 2: return 1 | withoutOrientation;
+                    case 0: return 2 | withoutOrientation;
+                    case 1: return 3 | withoutOrientation;
+                }
+
+            case PISTON_BASE:
+            case PISTON_STICKY_BASE:
+            case PISTON_EXTENSION:
+                final int rest = data & ~0x7;
+                switch (data & 0x7) {
+                    case 5: return 2 | rest;
+                    case 4: return 3 | rest;
+                    case 2: return 4 | rest;
+                    case 3: return 5 | rest;
+                }
+                break;
+
+            case HUGE_MUSHROOM_1:
+            case HUGE_MUSHROOM_2:
+                if (data >= 10) return data;
+                return (data * 7) % 10;
+
+            case VINE:
+                return ((data >> 1) | (data << 3)) & 0xf;
+
+            case FENCE_GATE:
+                return ((data + 3) & 0x3) | (data & ~0x3);
         }
 
         return data;
