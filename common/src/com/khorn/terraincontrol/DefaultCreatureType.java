c62bf0e32cfc769e61d2bb3ec6d13dbef8b6914d
Olof Larsson <olof@sylt.nu>
2012-03-24 12:49:13 +0100

    Added enum for creature types to common.

---
+++ common/src/com/khorn/terraincontrol/DefaultCreatureType.java
@@ -0,0 +1,93 @@
+package com.khorn.terraincontrol;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public enum DefaultCreatureType
+{
+    // The first strings MUST match the strings in nms.EntityTypes and are case sensitive.
+    CREEPER(50, "Creeper", "creeper"),
+    SKELETON(51, "Skeleton", "skeleton"),
+    SPIDER(52, "Spider", "spider"),
+    GIANT(53, "Giant", "giant", "giantzombie", "zombiegiant"),
+    ZOMBIE(54, "Zombie", "zombie"),
+    SLIME(55, "Slime", "slime"),
+    GHAST(56, "Ghast", "ghast"),
+    PIG_ZOMBIE(57, "PigZombie", "pigzombie", "pig_zombie"),
+    ENDERMAN(58, "Enderman", "enderman"),
+    CAVE_SPIDER(59, "CaveSpider", "cavespider", "cave_spider"),
+    SILVERFISH(60, "Silverfish", "silverfish", "silver_fish"),
+    BLAZE(61, "Blaze", "blaze"),
+    MAGMA_CUBE(62, "LavaSlime", "lavaslime", "lava_slime", "magmacube", "magma_cube"),
+    ENDER_DRAGON(63, "EnderDragon", "enderdragon", "ender_dragon"),
+    PIG(90, "Pig", "pig"),
+    SHEEP(91, "Sheep", "sheep"),
+    COW(92, "Cow", "cow"),
+    CHICKEN(93, "Chicken", "chicken"),
+    SQUID(94, "Squid", "squid"),
+    WOLF(95, "Wolf", "wolf"),
+    MUSHROOM_COW(96, "MushroomCow", "mushroomcow", "shroom", "mooshroom", "moshoom", "mcow", "shroomcow"),
+    SNOWMAN(97, "SnowMan", "snowman"),
+    OCELOT(98, "Ozelot", "ozelot"),
+    IRON_GOLEM(99, "VillagerGolem", "irongolem", "iron_golem"),
+    VILLAGER(120, "Villager", "villager");
+
+    // INSTANCE FIELDS
+    protected final short typeId;
+    public short getTypeId() { return this.typeId; }
+    
+    protected final String formalName;
+    public String getFormalName() { return this.formalName; }
+    
+    protected final String preferedName;
+    public String getPreferedName() { return this.preferedName; }
+    
+    protected final Set<String> allNames;
+    public Set<String> getAllNames() { return this.allNames; }
+
+    // LOOKUP MAPS
+    private static final Map<Short, DefaultCreatureType> ID_MAP = new HashMap<Short, DefaultCreatureType>();
+    private static final Map<String, DefaultCreatureType> NAME_MAP = new TreeMap<String, DefaultCreatureType>(String.CASE_INSENSITIVE_ORDER);
+
+    static
+    {
+        // Build the lookup maps
+        for (DefaultCreatureType type : EnumSet.allOf(DefaultCreatureType.class))
+        {
+            ID_MAP.put(type.typeId, type);
+            for (String name : type.allNames)
+            {
+                NAME_MAP.put(name, type);
+            }            
+        }
+    }
+
+    // The constructor
+    private DefaultCreatureType(int typeId, String... names)
+    {
+        this.typeId = (short) typeId;
+        this.formalName = names[0];
+        this.preferedName = names[1];
+        this.allNames = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+        this.allNames.addAll(Arrays.asList(names));
+    }
+
+    public static DefaultCreatureType fromName(String name) 
+    {
+        return NAME_MAP.get(name);
+    }
+
+    public static DefaultCreatureType fromId(int id)
+    {
+        if (id > Short.MAX_VALUE)
+        {
+            return null;
+        }
+        return ID_MAP.get((short) id);
+    }
+}
