3f4f45aadd40a92ac59f684f606dbe5e7b96226a
Rutger Kok <rutger.kok@live.com>
2012-11-19 19:37:51 +0100

    Fix #98 - colors don't update when switching worlds

--- bukkit/src/com/khorn/terraincontrol/bukkit/TCListener.java
+++ bukkit/src/com/khorn/terraincontrol/bukkit/TCListener.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.world.StructureGrowEvent;
 import org.bukkit.event.world.WorldInitEvent;
@@ -85,56 +86,19 @@ public class TCListener implements Listener
         }
     }
 
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event)
-	{
-		// Sends the packet
-
-		final Player player = event.getPlayer();
-
-		// Because the player doesn't send the REGISTER packet immediately, we
-		// have to wait a second (20 ticks).
-		Bukkit.getScheduler().runTaskLater(tcPlugin, new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				if (!player.getListeningPluginChannels().contains(TCDefaultValues.ChannelName.stringValue()))
-				{
-					// Player doesn't have TC, abort.
-					System.out.println("TerrainControl: player joined without TerrainControl");
-					return;
-				}
-
-				// Send the configs
-				World world = player.getWorld();
-
-				if (tcPlugin.worlds.containsKey(world.getUID()))
-				{
-					WorldConfig config = tcPlugin.worlds.get(world.getUID()).getSettings();
-
-					System.out.println("TerrainControl: config sent to player for world " + config.WorldName);
-					ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-					DataOutputStream stream = new DataOutputStream(outputStream);
-
-					try
-					{
-						stream.writeInt(TCDefaultValues.ProtocolVersion.intValue());
-						config.Serialize(stream);
-						stream.flush();
-
-					} catch (IOException e)
-					{
-						e.printStackTrace();
-					}
-
-					byte[] data = outputStream.toByteArray();
-
-					player.sendPluginMessage(tcPlugin, TCDefaultValues.ChannelName.stringValue(), data);
-				}
-			}
-		}, 20);
-
-	}
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        // Sends the packet
+        // Because the player doesn't send the REGISTER packet immediately, we
+        // have to wait a second (20 ticks).
+        Bukkit.getScheduler().runTaskLater(tcPlugin, new TCSender(tcPlugin, event.getPlayer()), 20);
+    }
+    
+    @EventHandler
+    public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
+        // Resends the packet so that the biomes are right again
+        Bukkit.getScheduler().runTaskLater(tcPlugin, new TCSender(tcPlugin, event.getPlayer()), 1);
+    }
 
 }
---
+++ bukkit/src/com/khorn/terraincontrol/bukkit/TCSender.java
@@ -0,0 +1,61 @@
+package com.khorn.terraincontrol.bukkit;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import com.khorn.terraincontrol.configuration.TCDefaultValues;
+import com.khorn.terraincontrol.configuration.WorldConfig;
+
+public class TCSender implements Runnable
+{
+    private Player player;
+    private TCPlugin plugin;
+    
+    public TCSender(TCPlugin plugin, Player player) {
+        this.plugin = plugin;
+        this.player = player;
+    }
+    
+    @Override
+    public void run()
+    {
+        if (!player.getListeningPluginChannels().contains(TCDefaultValues.ChannelName.stringValue()))
+        {
+            // Player doesn't have TC, abort.
+            System.out.println("TerrainControl: player joined without TerrainControl");
+            return;
+        }
+
+        // Send the configs
+        World world = player.getWorld();
+
+        if (plugin.worlds.containsKey(world.getUID()))
+        {
+            WorldConfig config = plugin.worlds.get(world.getUID()).getSettings();
+
+            System.out.println("TerrainControl: config sent to player for world " + config.WorldName);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            DataOutputStream stream = new DataOutputStream(outputStream);
+
+            try
+            {
+                stream.writeInt(TCDefaultValues.ProtocolVersion.intValue());
+                config.Serialize(stream);
+                stream.flush();
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            byte[] data = outputStream.toByteArray();
+
+            player.sendPluginMessage(plugin, TCDefaultValues.ChannelName.stringValue(), data);
+        }
+    }
+
+}
