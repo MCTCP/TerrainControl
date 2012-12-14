7b82d5f22f1b3800b57c8bde1e6f92242af407be
Rutger Kok <rutger.kok@live.com>
2012-12-14 15:06:51 +0100

    Possibly fix Forge saving in wrong location
    - Use logger to display messages on Forge, instead of System.out
    - Formatting

--- forge/src/com/khorn/terraincontrol/forge/TCPlugin.java
+++ forge/src/com/khorn/terraincontrol/forge/TCPlugin.java
@@ -1,6 +1,7 @@
 package com.khorn.terraincontrol.forge;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.logging.Level;
 
 import net.minecraft.server.MinecraftServer;
@@ -13,6 +14,7 @@ import com.khorn.terraincontrol.customobjects.BODefaultValues;
 import com.khorn.terraincontrol.util.Txt;
 
 import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Side;
 import cpw.mods.fml.common.Mod.Init;
@@ -34,7 +36,7 @@ public class TCPlugin implements TerrainControlEngine
     @Instance("TerrainControl")
     public static TCPlugin instance;
 
-    public final File terrainControlDirectory = new File("mods" + File.separator + "TerrainControl");
+    public File terrainControlDirectory;
     private TCWorldType worldType;
 
     @PreInit
@@ -48,17 +50,35 @@ public class TCPlugin implements TerrainControlEngine
     {
         // This is the place where the mod starts loading
 
-        // Start engine
-        TerrainControl.startEngine(this);
+        // Set the directory
+        try
+        {
+            Field minecraftDir = Loader.class.getDeclaredField("minecraftDir");
+            minecraftDir.setAccessible(true);
+            terrainControlDirectory = new File((File) minecraftDir.get(null), "mods" + File.separator + "TerrainControl");
+        } catch (Throwable e)
+        {
+            terrainControlDirectory = new File("mods" + File.separator + "TerrainControl");
+            System.out.println("Could not reflect the Minecraft directory, save location may be unpredicatble.");
+            e.printStackTrace();
+        }
+        
+        // Start TerrainControl engine
         TerrainControl.supportedBlockIds = 4095;
+        TerrainControl.startEngine(this);
+        
         // Register localization
         LanguageRegistry.instance().addStringLocalization("generator.TerrainControl", "TerrainControl");
+        
         // Register world type
         worldType = new TCWorldType(this, 4, "TerrainControl");
+        
         // Register channel
-        if(FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
+        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
+        {
             NetworkRegistry.instance().registerChannel(new PacketHandler(this), TCDefaultValues.ChannelName.stringValue());
         }
+        
         // Register player tracker
         GameRegistry.registerPlayerTracker(new PlayerTracker(this));
     }
@@ -73,28 +93,27 @@ public class TCPlugin implements TerrainControlEngine
     public LocalWorld getWorld(String name)
     {
         LocalWorld world = worldType.worldTC;
-        if(world == null)
+        if (world == null)
         {
             return null;
         }
         String worldName = MinecraftServer.getServer().worldServers[0].getSaveHandler().getSaveDirectoryName();
-        if(world.getName() == worldName)
+        if (world.getName() == worldName)
         {
             return world;
-        }
-        else
+        } else
         {
             // Outdated world stored
             worldType.worldTC = null;
             return null;
         }
-        
+
     }
 
     @Override
     public void log(Level level, String... messages)
     {
-        System.out.println("TerrainControl: " + Txt.implode(messages, ","));
+        FMLCommonHandler.instance().getFMLLogger().log(level, "[TerrainControl] " + Txt.implode(messages, ","));
     }
 
     @Override
@@ -102,5 +121,4 @@ public class TCPlugin implements TerrainControlEngine
     {
         return new File(terrainControlDirectory, BODefaultValues.BO_GlobalDirectoryName.stringValue());
     }
-
 }
