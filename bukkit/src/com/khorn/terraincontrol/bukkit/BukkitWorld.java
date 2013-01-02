MCTCP/TerrainControl

commit f3b8460533acd3fb04817edef96d0a634874d341

Fix #25 by reporting the correct sea level
- On Forge this is done by overriding a method in WorldType.
- On Bukkit, which doesn't use the world type system for custom
generators, it is done by overriding WorldProvider.

bukkit/src/com/khorn/terraincontrol/bukkit/BukkitWorld.java
@@ -566,8 +566,8 @@ public class BukkitWorld implements LocalWorld
         this.world = _world;
         this.seed = world.getSeed();
 
-        // TODO: Should WorldProviderTC extend even more? For example for spawn point etc?
-        // this.world.worldProvider = new TCWorldProvider().setSeaLevel(this.settings.waterLevelMax); // cause errors with entity burn, disabled temporary.
+        // TODO check for mob burning issues
+        this.world.worldProvider = new TCWorldProvider(this);
 
         this.chunkCache = new Chunk[4];
 

bukkit/src/com/khorn/terraincontrol/bukkit/TCWorldProvider.java
@@ -12,33 +12,22 @@ import net.minecraft.server.v1_4_6.WorldProvider;
  */
 public class TCWorldProvider extends WorldProvider
 {
-    protected int seaLevel = 64;
+    protected BukkitWorld localWorld;
+    
+    public TCWorldProvider(BukkitWorld localWorld)
+    {
+        this.localWorld = localWorld;
+    }
     
     @Override
     public int getSeaLevel()
     {
-        return this.seaLevel;
+        return localWorld.getSettings().waterLevelMax;
     }
-    
-    public TCWorldProvider setSeaLevel(int value)
-    {
-        if (value < 1)
-        {
-            this.seaLevel = 1;
-        }
-        else if (value > 256)
-        {
-            this.seaLevel = 256;
-        }
-        else
-        {
-            this.seaLevel = value;
-        }
-        return this;
-    }
-    
+
+    @Override
     public String getName()
     {
-        return "Terrain Control";
+        return "Overworld";
     }
 }

forge/src/com/khorn/terraincontrol/forge/TCWorldType.java
@@ -85,5 +85,11 @@ public class TCWorldType extends WorldType
         } else
             return super.getChunkGenerator(world, generatorOptions);
     }
-
+    
+    
+    @Override
+    public int getMinimumSpawnHeight(World world)
+    {
+        return WorldHelper.toLocalWorld(world).getSettings().waterLevelMax;
+    }
 }

commit c6df9196a10638e3eb287242dcfd24f415ffd4cd

Don't replace the Nether and The End world provider
Fixes the issues caused by f3b8460533acd3fb04817edef96d0a634874d341

bukkit/src/com/khorn/terraincontrol/bukkit/BukkitWorld.java
@@ -567,7 +567,13 @@ public class BukkitWorld implements LocalWorld
         this.seed = world.getSeed();
 
         // TODO check for mob burning issues
-        this.world.worldProvider = new TCWorldProvider(this);
+        if(this.world.worldProvider.getName().equals("Overworld"))
+        {
+            // Only replace the worldProvider if it's the overworld
+            // Replacing other dimensions causes a lot of glitches
+            this.world.worldProvider = new TCWorldProvider(this);
+        }
+        
 
         this.chunkCache = new Chunk[4];
 