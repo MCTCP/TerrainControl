7a38fcc372d0ce518da2c177015ea4016fcac89a
Rutger Kok <rutger.kok@live.com>
2013-01-13 19:53:41 +0100

    Use material name instead of id in BlockCheck

--- common/src/com/khorn/terraincontrol/customobjects/bo3/BlockCheck.java
+++ common/src/com/khorn/terraincontrol/customobjects/bo3/BlockCheck.java
@@ -69,7 +69,7 @@ public class BlockCheck extends BO3Check
         for (int i = 0; i < blockIds.size(); i++)
         {
             builder.append(',');
-            builder.append(blockIds.get(i));
+            builder.append(makeMaterial(blockIds.get(i)));
             if (blockDatas.get(i) != -1)
             {
                 builder.append('.');
