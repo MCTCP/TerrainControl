MCTCP/TerrainControl

commit 9c3d6b120c657b0ce541bd14f812adcd0e2a44f8

Change WorldConfig.biomeConfigs from HashMap to simple array for better performance. Forge version need testes. Fix wrong method name in forge.TCWorldChunkManager.

bukkit/src/com/khorn/terraincontrol/bukkit/MapWriter.java
@@ -68,8 +68,8 @@ public class MapWriter implements Runnable
             BukkitWorld bukkitWorld = plugin.worlds.get(world.getDataManager().getUUID());
             if (bukkitWorld != null)
             {
-                colors = new int[bukkitWorld.getSettings().highestBiomeId + 1];
-                for (BiomeConfig biomeConfig : bukkitWorld.getSettings().biomeConfigs.values())
+                colors = new int[bukkitWorld.getSettings().biomesCount + 1];
+                for (BiomeConfig biomeConfig : bukkitWorld.getSettings().biomeConfigs)
                 {
                     if (biomeConfig != null)
                     {

bukkit/src/com/khorn/terraincontrol/bukkit/BukkitWorld.java
@@ -271,7 +271,7 @@ public class BukkitWorld implements LocalWorld
                 {
                     for (int sectionZ = 0; sectionZ < 16; sectionZ++)
                     {
-                        BiomeConfig biomeConfig = this.settings.biomeConfigs.get(ChunkBiomes[(sectionZ << 4) | sectionX] & 0xFF);
+                        BiomeConfig biomeConfig = this.settings.biomeConfigs[ChunkBiomes[(sectionZ << 4) | sectionX] & 0xFF];
                         if (biomeConfig != null && biomeConfig.ReplaceCount > 0)
                         {
                             for (int sectionY = 0; sectionY < 16; sectionY++)

bukkit/src/com/khorn/terraincontrol/bukkit/TCListener.java
@@ -41,10 +41,10 @@ public class TCListener implements Listener
         int z = event.getLocation().getBlockZ();
 
         int biomeId = bukkitWorld.getCalculatedBiomeId(x, z);
-        if (bukkitWorld.getSettings().biomeConfigs.get(biomeId) == null)
+        if (bukkitWorld.getSettings().biomeConfigs[biomeId] == null)
             return;
 
-        BiomeConfig biomeConfig = bukkitWorld.getSettings().biomeConfigs.get(biomeId);
+        BiomeConfig biomeConfig = bukkitWorld.getSettings().biomeConfigs[biomeId];
         SaplingGen sapling;
 
         switch (event.getSpecies())

bukkit/src/com/khorn/terraincontrol/bukkit/TCWorldChunkManager.java
@@ -61,7 +61,7 @@ public class TCWorldChunkManager extends WorldChunkManager implements IBiomeMana
         int[] arrayOfInt = this.BiomeLayer.Calculate(paramInt1, paramInt2, paramInt3, paramInt4);
         for (int i = 0; i < paramInt3 * paramInt4; i++)
         {
-            float f1 = worldConfig.biomeConfigs.get(arrayOfInt[i]).getWetness() / 65536.0F;
+            float f1 = worldConfig.biomeConfigs[arrayOfInt[i]].getWetness() / 65536.0F;
             if (f1 < this.worldConfig.minMoisture)
                 f1 = this.worldConfig.minMoisture;
             if (f1 > this.worldConfig.maxMoisture)
@@ -84,7 +84,7 @@ public class TCWorldChunkManager extends WorldChunkManager implements IBiomeMana
         int[] arrayOfInt = this.BiomeLayer.Calculate(paramInt1, paramInt2, paramInt3, paramInt4);
         for (int i = 0; i < paramInt3 * paramInt4; i++)
         {
-            float f1 = worldConfig.biomeConfigs.get(arrayOfInt[i]).getTemperature() / 65536.0F;
+            float f1 = worldConfig.biomeConfigs[arrayOfInt[i]].getTemperature() / 65536.0F;
             if (f1 < this.worldConfig.minTemperature)
                 f1 = this.worldConfig.minTemperature;
             if (f1 > this.worldConfig.maxTemperature)

bukkit/src/com/khorn/terraincontrol/bukkit/structuregens/MineshaftGen.java
@@ -20,7 +20,7 @@ public class MineshaftGen extends StructureGenerator
         {
             LocalWorld world = WorldHelper.toLocalWorld(worldMC);
             int biomeId = world.getCalculatedBiomeId(chunkX * 16 + 8, chunkZ * 16 + 8);
-            if (rand.nextDouble() * 100.0 < world.getSettings().biomeConfigs.get(biomeId).mineshaftsRarity)
+            if (rand.nextDouble() * 100.0 < world.getSettings().biomeConfigs[biomeId].mineshaftsRarity)
             {
                 return true;
             }

bukkit/src/com/khorn/terraincontrol/bukkit/structuregens/NetherFortressGen.java
@@ -50,7 +50,7 @@ public class NetherFortressGen extends StructureGenerator
             {
                 LocalWorld world = WorldHelper.toLocalWorld(worldObj);
                 int biomeId = world.getCalculatedBiomeId(chunkX * 16 + 8, chunkZ * 16 + 8);
-                if (!world.getSettings().biomeConfigs.get(biomeId).netherFortressesEnabled)
+                if (!world.getSettings().biomeConfigs[biomeId].netherFortressesEnabled)
                 {
                     return false;
                 }

bukkit/src/com/khorn/terraincontrol/bukkit/structuregens/RareBuildingGen.java
@@ -35,8 +35,10 @@ public class RareBuildingGen extends StructureGenerator
     {
         biomeList = new ArrayList<BiomeBase>();
 
-        for (BiomeConfig biomeConfig : worldConfig.biomeConfigs.values())
+        for (BiomeConfig biomeConfig : worldConfig.biomeConfigs)
         {
+            if (biomeConfig == null)
+                continue;
             if (biomeConfig.rareBuildingType != RareBuildingType.disabled)
             {
                 biomeList.add(((BukkitBiome) biomeConfig.Biome).getHandle());
@@ -101,7 +103,7 @@ public class RareBuildingGen extends StructureGenerator
     /**
      * returns possible spawns for scattered features
      */
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings({"rawtypes", "UnusedDeclaration"})
     public List getScatteredFeatureSpawnList()
     {
         return this.scatteredFeatureSpawnList;

bukkit/src/com/khorn/terraincontrol/bukkit/structuregens/RareBuildingStart.java
@@ -13,7 +13,7 @@ public class RareBuildingStart extends StructureStart
     public RareBuildingStart(World world, Random random, int chunkX, int chunkZ)
     {
         LocalWorld localWorld = WorldHelper.toLocalWorld(world);
-        BiomeConfig biomeConfig = localWorld.getSettings().biomeConfigs.get(localWorld.getCalculatedBiomeId(chunkX * 16 + 8, chunkZ * 16 + 8));
+        BiomeConfig biomeConfig = localWorld.getSettings().biomeConfigs[localWorld.getCalculatedBiomeId(chunkX * 16 + 8, chunkZ * 16 + 8)];
         StructurePiece building;
         switch (biomeConfig.rareBuildingType)
         {

commit 4c83c5915696598001736cbd26552aeb81fc275c

Fix /tc map and custom biomes
Fix problems caused by 9c3d6b120c657b0ce541bd14f812adcd0e2a44f8

bukkit/src/com/khorn/terraincontrol/bukkit/MapWriter.java
@@ -68,7 +68,7 @@ public class MapWriter implements Runnable
             BukkitWorld bukkitWorld = plugin.worlds.get(world.getDataManager().getUUID());
             if (bukkitWorld != null)
             {
-                colors = new int[bukkitWorld.getSettings().biomesCount + 1];
+                colors = new int[bukkitWorld.getSettings().biomeConfigs.length];
                 for (BiomeConfig biomeConfig : bukkitWorld.getSettings().biomeConfigs)
                 {
                     if (biomeConfig != null)