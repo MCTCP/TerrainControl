MCTCP/TerrainControl

commit 01bc12431e577c905c4250c4b393569d3952ed57

Fix NBT-NPE, fix Sapling importing
- Fix NPE on nameless tags in the root
- Fix SaplingType(0,... not being converted to SaplingType(Oak,...)

common/src/com/khorn/terraincontrol/customobjects/bo3/BO3Loader.java
@@ -91,7 +91,7 @@ public class BO3Loader implements CustomObjectLoader
         Tag[] values = (Tag[]) metadata.getValue();
         for (Tag subTag : values)
         {
-            if (subTag.getName().equals("id") && subTag.getType().equals(Tag.Type.TAG_String))
+            if (subTag.getName() != null && subTag.equals("id") && subTag.getType().equals(Tag.Type.TAG_String))
             {
                 // Found id tag, so return the root tag
                 return metadata;

common/src/com/khorn/terraincontrol/generator/resourcegens/SaplingType.java
@@ -41,7 +41,7 @@ public enum SaplingType
     {
         for(SaplingType type: SaplingType.values())
         {
-            if(type.id > 0)
+            if(type.id >= 0)
             {
                 lookupList[type.id] = type;
             }

commit 8e7a6bb8999f958398627957d75c8c032455651e

Fix NBT tags in root
Bug was caused by 01bc12431e577c905c4250c4b393569d3952ed57.

bukkit/src/com/khorn/terraincontrol/bukkit/BukkitWorld.java
@@ -664,7 +664,7 @@ public class BukkitWorld implements LocalWorld
         nmsTag.setInt("z", z);
         // Add that data to the current tile entity in the world
         TileEntity tileEntity = world.getTileEntity(x, y, z);
-        if(tileEntity != null)
+        if (tileEntity != null)
         {
             tileEntity.a(nmsTag); // tileEntity.load
         } else

common/src/com/khorn/terraincontrol/customobjects/bo3/BO3Loader.java
@@ -84,14 +84,14 @@ public class BO3Loader implements CustomObjectLoader
         }
 
         // The file can be structured in two ways:
-        // 1. chest.nbt with all the contents direclty in it
+        // 1. chest.nbt with all the contents directly in it
         // 2. chest.nbt with a Compound tag in it with all the data
 
         // Check for type 1 by searching for an id tag
         Tag[] values = (Tag[]) metadata.getValue();
         for (Tag subTag : values)
         {
-            if (subTag.getName() != null && subTag.equals("id") && subTag.getType().equals(Tag.Type.TAG_String))
+            if (subTag.getName() != null && subTag.getName().equals("id") && subTag.getType().equals(Tag.Type.TAG_String))
             {
                 // Found id tag, so return the root tag
                 return metadata;