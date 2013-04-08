89f83cabf299cd4d0c636cb6023435172681fef0
Rutger Kok <rutger.kok@live.com>
2013-04-08 18:03:23 +0200

    Update version to 2.4.10

--- forge/src/com/khorn/terraincontrol/forge/TCPlugin.java
+++ forge/src/com/khorn/terraincontrol/forge/TCPlugin.java
@@ -26,7 +26,7 @@ import java.io.File;
 import java.lang.reflect.Field;
 import java.util.logging.Level;
 
-@Mod(modid = "TerrainControl", name = "TerrainControl", version = "2.4.9")
+@Mod(modid = "TerrainControl", name = "TerrainControl", version = "2.4.10")
 @NetworkMod(clientSideRequired = false, serverSideRequired = false, versionBounds = "*")
 public class TCPlugin implements TerrainControlEngine
 {
