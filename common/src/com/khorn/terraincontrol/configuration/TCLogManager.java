2978b9ff2bd30b0d4fc3d295679c47166939fc4d
Timethor <Nick.o.rich@gmail.com>
2013-08-24 03:30:00 -0400

    Fix: TerrainControl.ini not auto-gen on first use
    Also, some missed log formatting/verbage and a small fix for a previous
    change for log level checking

--- common/src/com/khorn/terraincontrol/configuration/TCLogManager.java
+++ common/src/com/khorn/terraincontrol/configuration/TCLogManager.java
@@ -31,18 +31,19 @@ public class TCLogManager
             {
                 spacer = "";
                 String levelName = record.getLevel().getName();
+                //>>	This part aligns the [TerrainControl] tags no matter what level of log we are displaying
                 if (levelName.equals(Level.ALL.getName()) || levelName.equals(Level.OFF.getName()))
                 {
-                    spacer += "    ";
-                } else if (levelName.equals(Level.FINE.getName()))
+                    spacer = "    ";
+                } else if (levelName.equals(Level.FINE.getName()) || levelName.equals(Level.INFO.getName()))
                 {
-                    spacer += "   ";
+                    spacer = "   ";
                 } else if (levelName.equals(Level.FINER.getName()))
                 {
-                    spacer += "  ";
+                    spacer = "  ";
                 } else if (levelName.equals(Level.CONFIG.getName()) || levelName.equals(Level.FINEST.getName()) || levelName.equals(Level.SEVERE.getName()))
                 {
-                    spacer += " ";
+                    spacer = " ";
                 }
                 return new StringBuilder()
                     .append(spacer).append("[").append(prefix).append("] ")
@@ -95,18 +96,11 @@ public class TCLogManager
                 handlerParentName = h.getClass().getSuperclass().getSimpleName();
                 if (handlerName.contains(ConsoleHandler.class.getSimpleName()) || handlerParentName.contains(ConsoleHandler.class.getSimpleName()))
                 {
-                    if (consoleLogLevel == LogLevels.Off)
-                    {
-                        h.setLevel(Level.WARNING);
-                    }
-
+                    h.setLevel(consoleLogLevel.getLevel());
                     logHandlerLevelSet(logger, h, consoleLogLevel);
                 } else if (handlerName.equals(FileHandler.class.getSimpleName()))
                 {
-                    if (fileLogLevel == LogLevels.Off)
-                    {
-                        h.setLevel(Level.WARNING);
-                    }
+                    h.setLevel(fileLogLevel.getLevel());
                     logHandlerLevelSet(logger, h, fileLogLevel);
                 }
             }
@@ -127,12 +121,8 @@ public class TCLogManager
     //t>>	Potential future site of addFileHandler(); to be configured by plugin config for plugin-specific Log file and level
     public static LogLevels clampLevel(LogLevels level)
     {
-        if (level.getLevel().intValue() > defaultLogLevel.getLevel().intValue())
+        if (level != PluginConfig.LogLevels.Off && level.getLevel().intValue() > defaultLogLevel.getLevel().intValue())
         {
-            TCLogManager.getLogger().log(Level.WARNING, " [{0}] FileLoglevel `{1}` is not a safe level, reverting to default.", new Object[]
-            {
-                TCDefaultValues.ChannelName.stringValue(), level.getLevel().getLocalizedName()
-            });
             return defaultLogLevel;
         }
         return level;
