MCTCP/TerrainControl

commit 0ee3a23fc2795bc823b6b702ba381c2187110dcf

Redo BiomeCache API
Previously, the BiomeGenerator required a BiomeCache object in the
constructor. The BiomeCache itself required the BiomeGenerator, creating
an interesting circular dependency, that could easily lead to
StackOverflowErrors.

Now the biome cache itselfs implements BiomeGenerator and wraps another
BiomeGenerator. The original BiomeGenerator has no caching logic left
anymore.

When calling BiomeModeManager.createCached the biome generator will
automatically be wrapped into a cache. However, the vanilla generator
(or any other generator that returns true from the isCached method)
won't be wrapped into a cache, as vanilla Minecraft uses its own biome
cache.

common/src/main/java/com/khorn/terraincontrol/generator/biome/BiomeGenerator.java
@@ -3,32 +3,42 @@ package com.khorn.terraincontrol.generator.biome;
 import com.khorn.terraincontrol.LocalWorld;
 
 /**
- * The biome generator. External plugins are allowed to implement this class on their own.
+ * The biome generator. External plugins are allowed to implement this class
+ * on their own as long as it is registered using
+ * {@link BiomeModeManager#register(String, Class)}.
  *
+ * <p>
+ * A biome generator doesn't have to implement all methods in this class. If
+ * it can't generate biomes at a lower precision, {@link
+ * #getBiomesUnZoomed(int[], int, int, int, int, OutputType)} doesn't have to
+ * be overridden. If it doesn't have its own cache, {@link #cleanupCache()}
+ * and {@link #getBiome(int, int)} also don't have to be overridden.
+ *
+ * <p>
+ * Two methods exist to detect whether the generator supports {@link
+ * #canGenerateUnZoomed() unzoomed biomes} and {@link #isCached() cached
+ * lookups}. By default, both methods return {@code false}. If the biome
+ * generator does support those features the methods must be overridden.
  */
 public abstract class BiomeGenerator
 {
-    protected LocalWorld world;
-    protected BiomeCache cache;
-    public final Object lockObject = new Object();
+    protected final LocalWorld world;
 
-    public BiomeGenerator(LocalWorld world, BiomeCache cache)
+    public BiomeGenerator(LocalWorld world)
     {
         this.world = world;
-        this.cache = cache;
     }
 
     /**
-     * Calculate biome ids array used in terrain generation.
+     * Calculates the biome ids array used in terrain generation.
      * 
      * @param biomeArray Output array. If it is null or wrong size return new
      *            array.
      * @param x The block x.
      * @param z The block z.
-     * @param x_size Size of block in x coordinate.
-     * @param z_size Size of blocks in z coordinate.
-     * @param type Output type. May be FULL, WITHOUT_RIVERS, ONLY_RIVERS or
-     *            DEFAULT_FOR_WORLD.
+     * @param xSize Size of block in x coordinate.
+     * @param zSize Size of blocks in z coordinate.
+     * @param type Output type.
      * @return Array filled by biome ids.
      */
     public int[] getBiomesUnZoomed(int[] biomeArray, int x, int z, int xSize, int zSize, OutputType type)
@@ -44,11 +54,53 @@ public abstract class BiomeGenerator
 
     public abstract int[] getBiomes(int[] biomeArray, int x, int z, int xSize, int zSize, OutputType type);
 
-    public abstract int getBiome(int x, int z);
+    /**
+     * Gets the biome of a single column. Only available for cached biome
+     * generators, as the method would be way too slow otherwise.
+     * @param blockX X coord of the column.
+     * @param blockZ Z coord of the column.
+     * @return The biome ud.
+     * @throws UnsupportedOperationException If {@link #isCached()} == false.
+     */
+    public int getBiome(int blockX, int blockZ) throws UnsupportedOperationException
+    {
+        if (isCached())
+        {
+            // Implementation has a bug
+            throw new AssertionError("isCached() == true, but getBiome is not overridden");
+        } else
+        {
+            throw new UnsupportedOperationException("isCached() == false, so no single biome lookups");
+        }
+    }
 
-    public abstract void cleanupCache();
+    /**
+     * Cleans up the cache.
+     * @throws UnsupportedOperationException If {@link #isCached()} == false.
+     */
+    public void cleanupCache() throws UnsupportedOperationException
+    {
+        if (isCached())
+        {
+            // Implementation has a bug
+            throw new AssertionError("isCached() == true, but cleanupCache is not overridden");
+        } else
+        {
+            throw new UnsupportedOperationException("isCached() == false, so no cache to cleanup");
+        }
+    }
 
     public boolean canGenerateUnZoomed()
+    {
+        return false;
+    }
+
+    /**
+     * Gets whether this biome generator is cached. Cached biome generators
+     * have an implementation for {@link #getBiome(int, int)} and {@link #cleanupCache()}.
+     * @return True if this biome generator is cached, false otherwise.
+     */
+    public boolean isCached()
     {
         return false;
     }

common/src/main/java/com/khorn/terraincontrol/generator/biome/CachedBiomeGenerator.java
@@ -1,0 +1,182 @@
+package com.khorn.terraincontrol.generator.biome;
+
+import com.khorn.terraincontrol.LocalWorld;
+import com.khorn.terraincontrol.util.ChunkCoordinate;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * Wraps uncached biome generators.
+ * @see BiomeModeManager#createCached(Class, LocalWorld)
+ */
+class CachedBiomeGenerator extends BiomeGenerator
+{
+    /**
+     * Caches the biomes of a single chunk.
+     *
+     */
+    private static class Block
+    {
+        /**
+         * The array of biome types stored in this BiomeCache.Block.
+         */
+        int[] biomes = new int[256];
+        /**
+         * The last time this BiomeCacheBlock was accessed, in milliseconds.
+         */
+        long lastAccessTime;
+
+        Block(BiomeGenerator generator, ChunkCoordinate chunkCoord)
+        {
+            biomes = generator.getBiomes(null, chunkCoord.getBlockX(), chunkCoord.getBlockZ(), ChunkCoordinate.CHUNK_X_SIZE,
+                    ChunkCoordinate.CHUNK_Z_SIZE, OutputType.DEFAULT_FOR_WORLD);
+        }
+
+        /**
+         * Gets the biome type id of the column at the given location.
+         * @param blockX X location of the column, must fall in this cache
+         *               block.
+         * @param blockZ Z location of the column, must fall in this cache
+         *               block.
+         * @return The biome type id.
+         */
+        int getCalculatedBiomeId(int blockX, int blockZ)
+        {
+            return biomes[blockX & 15 | (blockZ & 15) << 4];
+        }
+    }
+
+    /**
+     * The map of cached BiomeCacheBlocks.
+     */
+    private Map<ChunkCoordinate, CachedBiomeGenerator.Block> cacheMap = new HashMap<ChunkCoordinate, Block>();
+    /**
+     * The uncached biome generator.
+     */
+    private final BiomeGenerator generator;
+    /**
+     * The last time this BiomeCache was cleaned, in milliseconds.
+     */
+    private long lastCleanupTime;
+
+    private CachedBiomeGenerator(BiomeGenerator generator)
+    {
+        super(generator.world);
+        this.generator = generator;
+    }
+
+    /**
+     * Gets a cached generator that generates biomes like the given generator.
+     * If the given generator is already cached, it is returned immediately.
+     * If it isn't cached it is wrapped inside a {@link CachedBiomeGenerator}.
+     * @param generator A potentially uncached biome generator.
+     * @return A cached biome generator.
+     * @see BiomeModeManager#createCached(Class, LocalWorld)
+     */
+    static BiomeGenerator makeCached(BiomeGenerator generator)
+    {
+        if (generator.isCached())
+        {
+            return generator;
+        }
+        return new CachedBiomeGenerator(generator);
+    }
+
+    @Override
+    public void cleanupCache()
+    {
+        long currentTime = System.currentTimeMillis();
+        long timeSinceLastCleanup = currentTime - this.lastCleanupTime;
+
+        if (timeSinceLastCleanup < 7500L && timeSinceLastCleanup > 0L)
+        {
+            return;
+        }
+
+        this.lastCleanupTime = currentTime;
+
+        for (Iterator<Entry<ChunkCoordinate, CachedBiomeGenerator.Block>> it = cacheMap.entrySet().iterator(); it.hasNext();)
+        {
+            Entry<ChunkCoordinate, CachedBiomeGenerator.Block> entry = it.next();
+            CachedBiomeGenerator.Block block = entry.getValue();
+            long timeSinceLastAccessed = currentTime - block.lastAccessTime;
+
+            if (timeSinceLastAccessed > 30000L || timeSinceLastAccessed < 0L)
+            {
+                it.remove();
+            }
+        }
+
+    }
+
+    @Override
+    public int getBiome(int x, int z)
+    {
+        ChunkCoordinate chunkCoord = ChunkCoordinate.fromBlockCoords(x, z);
+        CachedBiomeGenerator.Block cacheBlock = getBiomeCacheBlock(chunkCoord);
+        return cacheBlock.getCalculatedBiomeId(x, z);
+    }
+
+    /**
+     * Returns a biome cache block at location specified.
+     */
+    private CachedBiomeGenerator.Block getBiomeCacheBlock(ChunkCoordinate chunkCoord)
+    {
+        CachedBiomeGenerator.Block block = this.cacheMap.get(chunkCoord);
+
+        if (block == null)
+        {
+            block = new CachedBiomeGenerator.Block(generator, chunkCoord);
+            this.cacheMap.put(chunkCoord, block);
+        }
+
+        block.lastAccessTime = System.currentTimeMillis();
+        return block;
+    }
+
+    @Override
+    public int[] getBiomes(int[] biomeArray, int x, int z, int xSize, int zSize, OutputType type)
+    {
+        if (xSize == ChunkCoordinate.CHUNK_X_SIZE && zSize == ChunkCoordinate.CHUNK_Z_SIZE && (x & 0xF) == 0 && (z & 0xF) == 0)
+        {
+            return getCachedBiomes(ChunkCoordinate.fromBlockCoords(x, z));
+        }
+        return generator.getBiomes(biomeArray, x, z, xSize, zSize, type);
+    }
+
+    @Override
+    public int[] getBiomesUnZoomed(int[] biomeArray, int x, int z, int xSize, int zSize, OutputType type)
+    {
+        return generator.getBiomesUnZoomed(biomeArray, x, z, xSize, zSize, type);
+    }
+
+    @Override
+    public boolean canGenerateUnZoomed()
+    {
+        return generator.canGenerateUnZoomed();
+    }
+
+    /**
+     * Returns the array of cached biome types in the BiomeCacheBlock at the
+     * given location.
+     */
+    public int[] getCachedBiomes(ChunkCoordinate chunkCoord)
+    {
+        return this.getBiomeCacheBlock(chunkCoord).biomes;
+    }
+
+    @Override
+    public float[] getRainfall(float[] paramArrayOfFloat, int x, int z, int xSize, int zSize)
+    {
+        return generator.getRainfall(paramArrayOfFloat, x, z, xSize, zSize);
+    }
+
+    @Override
+    public boolean isCached()
+    {
+        return true;
+    }
+}

platforms/bukkit/src/main/java/com/khorn/terraincontrol/bukkit/BukkitWorld.java
@@ -12,7 +12,6 @@ import com.khorn.terraincontrol.configuration.WorldConfig;
 import com.khorn.terraincontrol.configuration.WorldSettings;
 import com.khorn.terraincontrol.customobjects.CustomObjectStructureCache;
 import com.khorn.terraincontrol.exception.BiomeNotFoundException;
-import com.khorn.terraincontrol.generator.biome.BiomeCache;
 import com.khorn.terraincontrol.generator.biome.BiomeGenerator;
 import com.khorn.terraincontrol.generator.biome.OldBiomeGenerator;
 import com.khorn.terraincontrol.generator.biome.VanillaBiomeGenerator;
@@ -623,8 +622,7 @@ public class BukkitWorld implements LocalWorld
 
         // Inject our own BiomeManager (called WorldChunkManager)
         Class<? extends BiomeGenerator> biomeModeClass = this.settings.worldConfig.biomeMode;
-        biomeGenerator = TerrainControl.getBiomeModeManager().create(biomeModeClass, this,
-                new BiomeCache(this));
+        biomeGenerator = TerrainControl.getBiomeModeManager().createCached(biomeModeClass, this);
         injectWorldChunkManager(biomeGenerator);
 
         if (!initialized)

common/src/main/java/com/khorn/terraincontrol/generator/biome/BiomeCache.java
@@ -1,133 +1,0 @@
-package com.khorn.terraincontrol.generator.biome;
-
-import com.khorn.terraincontrol.LocalWorld;
-import com.khorn.terraincontrol.util.ChunkCoordinate;
-
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-
-/**
- * Caches biomes to speed up the biome generator.
- *
- */
-public class BiomeCache
-{
-    /** Reference to the world */
-    private final LocalWorld world;
-    /** The last time this BiomeCache was cleaned, in milliseconds. */
-    private long lastCleanupTime;
-    /**
-     * The map of cached BiomeCacheBlocks.
-     */
-    private Map<ChunkCoordinate, BiomeCache.Block> cacheMap = new HashMap<ChunkCoordinate, Block>();
-
-    public BiomeCache(LocalWorld world)
-    {
-        this.world = world;
-    }
-
-    /**
-     * Returns a biome cache block at location specified.
-     */
-    private BiomeCache.Block getBiomeCacheBlock(ChunkCoordinate chunkCoord)
-    {
-        BiomeCache.Block block = (BiomeCache.Block) this.cacheMap.get(chunkCoord);
-
-        if (block == null)
-        {
-            block = new BiomeCache.Block(world.getBiomeGenerator(), chunkCoord);
-            this.cacheMap.put(chunkCoord, block);
-        }
-
-        block.lastAccessTime = System.currentTimeMillis();
-        return block;
-    }
-
-    /**
-     * Gets the id of the biome at the given location.
-     *
-     * @param blockX The x position of the block.
-     * @param blockZ The z position of the block.
-     * @return The id of the biome.
-     */
-    public int getCalculatedBiomeId(int blockX, int blockZ) {
-        ChunkCoordinate chunkCoord = ChunkCoordinate.fromBlockCoords(blockX, blockZ);
-        BiomeCache.Block cacheBlock = getBiomeCacheBlock(chunkCoord);
-        return cacheBlock.getCalculatedBiomeId(blockX, blockZ);
-    }
-
-    /**
-     * Removes BiomeCacheBlocks from this cache that haven't been accessed in at
-     * least 30 seconds.
-     */
-    public void cleanupCache()
-    {
-        long currentTime = System.currentTimeMillis();
-        long timeSinceLastCleanup = currentTime - this.lastCleanupTime;
-
-        if (timeSinceLastCleanup < 7500L && timeSinceLastCleanup > 0L)
-        {
-            return;
-        }
-
-        this.lastCleanupTime = currentTime;
-
-        for (Iterator<Entry<ChunkCoordinate, BiomeCache.Block>> it = cacheMap.entrySet().iterator(); it.hasNext();)
-        {
-            Entry<ChunkCoordinate, BiomeCache.Block> entry = it.next();
-            BiomeCache.Block block = entry.getValue();
-            long timeSinceLastAccessed = currentTime - block.lastAccessTime;
-
-            if (timeSinceLastAccessed > 30000L || timeSinceLastAccessed < 0L)
-            {
-                it.remove();
-            }
-        }
-
-    }
-
-    /**
-     * Returns the array of cached biome types in the BiomeCacheBlock at the
-     * given location.
-     */
-    public int[] getCachedBiomes(ChunkCoordinate chunkCoord)
-    {
-        return this.getBiomeCacheBlock(chunkCoord).biomes;
-    }
-
-    /**
-     * Caches the biomes of a single chunk.
-     *
-     */
-    private static class Block
-    {
-        /**
-         * The array of biome types stored in this BiomeCache.Block.
-         */
-        int[] biomes = new int[256];
-        /**
-         * The last time this BiomeCacheBlock was accessed, in milliseconds.
-         */
-        long lastAccessTime;
-
-        Block(BiomeGenerator generator, ChunkCoordinate chunkCoord)
-        {
-            biomes = generator.getBiomes(null, chunkCoord.getBlockX(), chunkCoord.getBlockZ(), ChunkCoordinate.CHUNK_X_SIZE, ChunkCoordinate.CHUNK_Z_SIZE, OutputType.DEFAULT_FOR_WORLD);
-        }
-
-        /**
-         * Gets the biome type id of the column at the given location.
-         * @param blockX X location of the column, must fall in this cache
-         *               block.
-         * @param blockZ Z location of the column, must fall in this cache
-         *               block.
-         * @return The biome type id.
-         */
-        int getCalculatedBiomeId(int blockX, int blockZ)
-        {
-            return biomes[blockX & 15 | (blockZ & 15) << 4];
-        }
-    }
-}

common/src/main/java/com/khorn/terraincontrol/generator/biome/BiomeModeManager.java
@@ -56,28 +56,38 @@ public class BiomeModeManager
     }
 
     /**
-     * Does the reflection logic for you.
-     * <p/>
+     * Creates an instance of the given biome generator. If an error occurs
+     * during instantiation, a message is logged and the normal biome
+     * generator is returned.
      * @param clazz The BiomeGenerator class to instantiate.
      * @param world The world of the biome generator.
-     * @param cache The biome cache object.
-     * <p/>
-     * @return The instantiated object.
+     * @return The biome generator.
+     * @see #createCached(Class, LocalWorld)
      */
-    public <T extends BiomeGenerator> BiomeGenerator create(Class<T> clazz, LocalWorld world, BiomeCache cache)
+    public <T extends BiomeGenerator> BiomeGenerator create(Class<T> clazz, LocalWorld world)
     {
         try
         {
-            return clazz.getConstructor(new Class[]
-            {
-                LocalWorld.class, BiomeCache.class
-            }).newInstance(world, cache);
+            return clazz.getConstructor(LocalWorld.class).newInstance(world);
         } catch (Exception e)
         {
             TerrainControl.log(LogMarker.FATAL, "Cannot properly reflect biome manager, falling back on BiomeMode:Normal");
             TerrainControl.printStackTrace(LogMarker.FATAL, e);
-            return new NormalBiomeGenerator(world, cache);
+            return new NormalBiomeGenerator(world);
         }
+    }
+
+    /**
+     * Same as {@link #create(Class, LocalWorld)}, but the returned biome
+     * generator is now guaranteed to be cached: if the generator with the
+     * given class isn't cached, it is wrapped in a cache.
+     * @param biomeModeClass Class to create a biome generator from.
+     * @param world          World the biome generates for.
+     * @return The cached biome generator.
+     */
+    public BiomeGenerator createCached(Class<? extends BiomeGenerator> biomeModeClass, LocalWorld world)
+    {
+        return CachedBiomeGenerator.makeCached(create(biomeModeClass, world));
     }
 
     /**

common/src/main/java/com/khorn/terraincontrol/generator/biome/FromImageBiomeGenerator.java
@@ -8,8 +8,8 @@ import com.khorn.terraincontrol.LocalWorld;
  */
 public class FromImageBiomeGenerator extends NormalBiomeGenerator
 {
-    public FromImageBiomeGenerator(LocalWorld world, BiomeCache cache)
+    public FromImageBiomeGenerator(LocalWorld world)
     {
-        super(world, cache);
+        super(world);
     }
 }

common/src/main/java/com/khorn/terraincontrol/generator/biome/NormalBiomeGenerator.java
@@ -3,7 +3,6 @@ package com.khorn.terraincontrol.generator.biome;
 import com.khorn.terraincontrol.LocalWorld;
 import com.khorn.terraincontrol.configuration.WorldConfig;
 import com.khorn.terraincontrol.generator.biome.layers.Layer;
-import com.khorn.terraincontrol.util.ChunkCoordinate;
 
 /**
  * This is the normal biome mode, which has all of Terrain Control's features.
@@ -14,9 +13,9 @@ public class NormalBiomeGenerator extends BiomeGenerator
     private Layer biomeLayer;
     private OutputType defaultOutputType = OutputType.FULL;
 
-    public NormalBiomeGenerator(LocalWorld world, BiomeCache cache)
+    public NormalBiomeGenerator(LocalWorld world)
     {
-        super(world, cache);
+        super(world);
 
         Layer[] layers = Layer.Init(world.getSeed(), world);
 
@@ -76,21 +75,11 @@ public class NormalBiomeGenerator extends BiomeGenerator
     @Override
     public int[] getBiomes(int[] biomeArray, int x, int z, int x_size, int z_size, OutputType outputType)
     {
-        boolean useCache = true;
         if ((biomeArray == null) || (biomeArray.length < x_size * z_size))
         {
             biomeArray = new int[x_size * z_size];
-            useCache = false;
         }
 
-        if (useCache && (x_size == ChunkCoordinate.CHUNK_X_SIZE) && (z_size == ChunkCoordinate.CHUNK_Z_SIZE) && ((x & 0xF) == 0) && ((z & 0xF) == 0))
-        {
-            synchronized (this.lockObject)
-            {
-                biomeArray = this.cache.getCachedBiomes(ChunkCoordinate.fromBlockCoords(x, z));
-            }
-            return biomeArray;
-        }
         ArraysCache cache = ArraysCacheManager.GetCache();
         if (outputType == OutputType.DEFAULT_FOR_WORLD)
             cache.outputType = defaultOutputType;
@@ -103,18 +92,6 @@ public class NormalBiomeGenerator extends BiomeGenerator
 
         return biomeArray;
 
-    }
-
-    @Override
-    public int getBiome(int x, int z)
-    {
-        return cache.getCalculatedBiomeId(x, z);
-    }
-
-    @Override
-    public void cleanupCache()
-    {
-        cache.cleanupCache();
     }
 
     @Override

common/src/main/java/com/khorn/terraincontrol/generator/biome/OldBiomeGenerator.java
@@ -4,7 +4,6 @@ package com.khorn.terraincontrol.generator.biome;
 import com.khorn.terraincontrol.LocalWorld;
 import com.khorn.terraincontrol.configuration.WorldConfig;
 import com.khorn.terraincontrol.generator.noise.NoiseGeneratorOldOctaves;
-import com.khorn.terraincontrol.util.ChunkCoordinate;
 import com.khorn.terraincontrol.util.minecraftTypes.DefaultBiome;
 
 import java.util.Random;
@@ -23,9 +22,9 @@ public class OldBiomeGenerator extends BiomeGenerator
     private static int[] biomeDiagram = new int[4096];
     private static boolean hasGeneratedBiomeDiagram;
 
-    public OldBiomeGenerator(LocalWorld world, BiomeCache cache)
+    public OldBiomeGenerator(LocalWorld world)
     {
-        super(world, cache);
+        super(world);
         this.temperatureGenerator1 = new NoiseGeneratorOldOctaves(new Random(world.getSeed() * 9871L), 4);
         this.wetnessGenerator = new NoiseGeneratorOldOctaves(new Random(world.getSeed() * 39811L), 4);
         this.temperatureGenerator2 = new NoiseGeneratorOldOctaves(new Random(world.getSeed() * 543321L), 2);
@@ -46,7 +45,7 @@ public class OldBiomeGenerator extends BiomeGenerator
         }
         //???>>	Is this needed? I cant find a usage...
         int[] temp_biomeBases = new int[x_size * z_size];
-        this.getBiomes(temp_biomeBases, x, z, x_size, z_size, false);
+        this.getBiomes(temp_biomeBases, x, z, x_size, z_size);
 
         for (int i = 0; i < temp_out.length; i++)
         {
@@ -56,17 +55,11 @@ public class OldBiomeGenerator extends BiomeGenerator
         return temp_out;
     }
 
-    public int[] getBiomes(int[] paramArrayOfBiomeBase, int x, int z, int xSize, int zSize, boolean useCache)
+    public int[] getBiomes(int[] paramArrayOfBiomeBase, int x, int z, int xSize, int zSize)
     {
         if ((paramArrayOfBiomeBase == null) || (paramArrayOfBiomeBase.length < xSize * zSize))
         {
             paramArrayOfBiomeBase = new int[xSize * zSize];
-        }
-        if ((useCache) && (xSize == ChunkCoordinate.CHUNK_X_SIZE) && (zSize == ChunkCoordinate.CHUNK_Z_SIZE) && ((x & 0xF) == 0) && ((z & 0xF) == 0))
-        {
-            int[] localObject = this.cache.getCachedBiomes(ChunkCoordinate.fromBlockCoords(x, z));
-            System.arraycopy(localObject, 0, paramArrayOfBiomeBase, 0, xSize * zSize);
-            return paramArrayOfBiomeBase;
         }
 
         WorldConfig worldConfig = world.getSettings().worldConfig;
@@ -119,19 +112,7 @@ public class OldBiomeGenerator extends BiomeGenerator
     @Override
     public int[] getBiomes(int[] biomeArray, int x, int z, int x_size, int z_size, OutputType outputType)
     {
-        return getBiomes(biomeArray, x, z, x_size, z_size, false);
-    }
-
-    @Override
-    public int getBiome(int x, int z)
-    {
-        return this.cache.getCalculatedBiomeId(x, z);
-    }
-
-    @Override
-    public void cleanupCache()
-    {
-        this.cache.cleanupCache();
+        return getBiomes(biomeArray, x, z, x_size, z_size);
     }
 
     private static int getBiomeFromDiagram(double temp, double rain)

commit 3956e9925acdc39af834d899d1f7eab069ed1a72

Fix BiomeMode: OldGenerator being broken by 0ee3a23fc2795bc823b6b702ba381c2187110dcf

common/src/main/java/com/khorn/terraincontrol/LocalWorld.java
@@ -89,9 +89,6 @@ public interface LocalWorld
      */
     public LocalBiome getBiome(int x, int z) throws BiomeNotFoundException;
 
-    // temperature*rain
-    public double getBiomeFactorForOldBM(int index);
-
     // Default generators
     public void prepareDefaultStructures(int chunkX, int chunkZ, boolean dry);
 

common/src/main/java/com/khorn/terraincontrol/generator/ChunkProviderTC.java
@@ -4,13 +4,12 @@ import static com.khorn.terraincontrol.util.ChunkCoordinate.CHUNK_X_SIZE;
 import static com.khorn.terraincontrol.util.ChunkCoordinate.CHUNK_Y_SIZE;
 import static com.khorn.terraincontrol.util.ChunkCoordinate.CHUNK_Z_SIZE;
 
-import com.khorn.terraincontrol.generator.biome.BiomeGenerator;
-
 import com.khorn.terraincontrol.LocalWorld;
-import com.khorn.terraincontrol.TerrainControl;
 import com.khorn.terraincontrol.configuration.BiomeConfig;
 import com.khorn.terraincontrol.configuration.WorldConfig;
 import com.khorn.terraincontrol.configuration.WorldSettings;
+import com.khorn.terraincontrol.generator.biome.BiomeGenerator;
+import com.khorn.terraincontrol.generator.biome.OldBiomeGenerator;
 import com.khorn.terraincontrol.generator.biome.OutputType;
 import com.khorn.terraincontrol.generator.noise.NoiseGeneratorNewOctaves;
 import com.khorn.terraincontrol.generator.noise.NoiseGeneratorPerlinOctaves;
@@ -541,10 +540,13 @@ public class ChunkProviderTC
 
     private void oldBiomeFactor(int x, int z, int i4, int ySections, double noiseHeight)
     {
-        if (this.worldSettings.worldConfig.biomeMode == TerrainControl.getBiomeModeManager().OLD_GENERATOR)
+        BiomeGenerator unwrapped = localWorld.getBiomeGenerator().unwrap();
+        if (unwrapped instanceof OldBiomeGenerator)
         {
-            this.volatilityFactor = (1.0D - this.localWorld.getBiomeFactorForOldBM(z * 48 + 17 + x * 3));
-
+            OldBiomeGenerator oldBiomeGenerator = (OldBiomeGenerator) unwrapped;
+            int index = z * 48 + 17 + x * 3;
+            double product = oldBiomeGenerator.oldTemperature1[index] * oldBiomeGenerator.oldWetness[index];
+            this.volatilityFactor = 1.0 - product;
         } else
         {
             final BiomeConfig biomeConfig = toBiomeConfig(this.biomeArray[(x + this.maxSmoothRadius + (z + this.maxSmoothRadius)

common/src/main/java/com/khorn/terraincontrol/generator/biome/BiomeGenerator.java
@@ -105,4 +105,15 @@ public abstract class BiomeGenerator
         return false;
     }
 
+    /**
+     * Biome generators can be wrapped, for example to cache another biome
+     * generator. This method returns the unwrapped biome generator. If this
+     * biome generator doesn't wrap another biome generator, it returns itself.
+     * @return The unwrapped biome generator.
+     */
+    public BiomeGenerator unwrap()
+    {
+        return this;
+    }
+
 }

common/src/main/java/com/khorn/terraincontrol/generator/biome/CachedBiomeGenerator.java
@@ -179,4 +179,10 @@ class CachedBiomeGenerator extends BiomeGenerator
     {
         return true;
     }
+
+    @Override
+    public BiomeGenerator unwrap()
+    {
+        return generator.unwrap();
+    }
 }

platforms/bukkit/src/main/java/com/khorn/terraincontrol/bukkit/BukkitWorld.java
@@ -14,7 +14,6 @@ import com.khorn.terraincontrol.configuration.WorldSettings;
 import com.khorn.terraincontrol.customobjects.CustomObjectStructureCache;
 import com.khorn.terraincontrol.exception.BiomeNotFoundException;
 import com.khorn.terraincontrol.generator.biome.BiomeGenerator;
-import com.khorn.terraincontrol.generator.biome.OldBiomeGenerator;
 import com.khorn.terraincontrol.logging.LogMarker;
 import com.khorn.terraincontrol.util.ChunkCoordinate;
 import com.khorn.terraincontrol.util.NamedBinaryTag;
@@ -158,13 +157,6 @@ public class BukkitWorld implements LocalWorld
     public int getCalculatedBiomeId(int x, int z)
     {
         return this.biomeGenerator.getBiome(x, z);
-    }
-
-    @Override
-    public double getBiomeFactorForOldBM(int index)
-    {
-        OldBiomeGenerator oldBiomeGenerator = (OldBiomeGenerator) this.biomeGenerator;
-        return oldBiomeGenerator.oldTemperature1[index] * oldBiomeGenerator.oldWetness[index];
     }
 
     @Override

platforms/forge/src/main/java/com/khorn/terraincontrol/forge/ForgeWorld.java
@@ -10,7 +10,6 @@ import com.khorn.terraincontrol.forge.generator.ChunkProvider;
 import com.khorn.terraincontrol.forge.generator.structure.*;
 import com.khorn.terraincontrol.forge.util.NBTHelper;
 import com.khorn.terraincontrol.generator.biome.BiomeGenerator;
-import com.khorn.terraincontrol.generator.biome.OldBiomeGenerator;
 import com.khorn.terraincontrol.logging.LogMarker;
 import com.khorn.terraincontrol.util.ChunkCoordinate;
 import com.khorn.terraincontrol.util.NamedBinaryTag;
@@ -180,13 +179,6 @@ public class ForgeWorld implements LocalWorld
     public int getCalculatedBiomeId(int x, int z)
     {
         return this.biomeManager.getBiome(x, z);
-    }
-
-    @Override
-    public double getBiomeFactorForOldBM(int index)
-    {
-        OldBiomeGenerator oldBiomeGenerator = (OldBiomeGenerator) this.biomeManager;
-        return oldBiomeGenerator.oldTemperature1[index] * oldBiomeGenerator.oldWetness[index];
     }
 
     @Override