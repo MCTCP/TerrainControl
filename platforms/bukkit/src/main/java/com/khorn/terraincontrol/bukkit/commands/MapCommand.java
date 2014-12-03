00aab2442fb5fd4816b278181fa85ba790aa7a3c
rutgerkok <rutger.kok@live.com>
2014-12-03 21:26:54 +0100

    Put /tc map on the main thread
    This change was already made on the BiomeGroups branch. This should make
    the biome mapper more reliable on the main branch too, at the cost of
    pausing the server for a while.

--- platforms/bukkit/src/main/java/com/khorn/terraincontrol/bukkit/commands/MapCommand.java
+++ platforms/bukkit/src/main/java/com/khorn/terraincontrol/bukkit/commands/MapCommand.java
@@ -111,7 +111,7 @@ public class MapCommand extends BaseCommand
 
         MapWriter map = new MapWriter(world.getHandle(), size, angle, sender, offsetX, offsetZ, label);
 
-        this.plugin.getServer().getScheduler().runTaskAsynchronously(this.plugin, map);
+        this.plugin.getServer().getScheduler().runTask(this.plugin, map);
 
         return true;
     }
