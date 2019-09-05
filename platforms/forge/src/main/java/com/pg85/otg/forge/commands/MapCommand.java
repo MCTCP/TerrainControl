034bdc74f20dfdb72c8790a8072622579e207571
coll1234567 <joshl5324@gmail.com>
2019-09-05 12:39:10 -0400

    Forge command refactor.

---
+++ platforms/forge/src/main/java/com/pg85/otg/forge/commands/MapCommand.java
@@ -0,0 +1,81 @@
+package com.pg85.otg.forge.commands;
+
+import java.util.List;
+
+import com.pg85.otg.forge.commands.runnables.MapWriter;
+
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+
+public class MapCommand extends BaseCommand {
+	MapCommand() {
+		super();
+		name = "map";
+		usage = "map [World] [-s size] [-r rotate_angle] [-o offsetX offsetZ] [-l (add coordinate label to filename)]";
+		description = "Create biome and temperature map files";
+	}
+
+	@Override
+	public boolean onCommand(ICommandSender sender, List<String> args) {
+		
+		World world = sender.getEntityWorld();
+		int size = 200;
+		int offsetX = 0;
+		int offsetZ = 0;
+		MapWriter.Angle angle = MapWriter.Angle.d0;
+		String label = "";
+
+		for (int i = 0; i < args.size(); i++) {
+			if (args.get(i).equals("-s")) {
+				try {
+					size = Integer.parseInt(args.get(i + 1));
+				} catch (Exception e) {
+					sender.sendMessage(new TextComponentString(ERROR_COLOR + "Wrong size " + args.get(i + 1)));
+				}
+			}
+			if (args.get(i).equals("-o")) {
+				try {
+					offsetX = Integer.parseInt(args.get(i + 1));
+					offsetZ = Integer.parseInt(args.get(i + 2));
+				} catch (Exception e) {
+					sender.sendMessage(new TextComponentString(ERROR_COLOR + "Wrong size " + args.get(i + 1)));
+				}
+			}
+			if (args.get(i).equals("-r")) {
+				try {
+					int degrees = Integer.parseInt(args.get(i + 1));
+					if (degrees % 90 == 0) {
+						switch (degrees) {
+						case 90:
+							angle = MapWriter.Angle.d90;
+							break;
+						case 180:
+							angle = MapWriter.Angle.d180;
+							break;
+						case 270:
+							angle = MapWriter.Angle.d270;
+							break;
+						}
+					} else {
+						sender.sendMessage(
+								new TextComponentString(ERROR_COLOR + "Angles must be divisible by 90 degrees"));
+					}
+				} catch (Exception e) {
+					sender.sendMessage(new TextComponentString(ERROR_COLOR + "Wrong angle " + args.get(i + 1)));
+				}
+			}
+			if (args.get(i).equals("-l")) {
+				label = "[" + offsetX + "_" + offsetZ + "]";
+			}
+		}
+
+		MapWriter map = new MapWriter(world, size, angle, sender, offsetX, offsetZ, label);
+
+		// We don't have the runnable system from bukkit, so we run it all at once for
+		// now. Will cause a noticeable lag spike
+		map.run();
+		
+		return true;
+	}
+}
---
+++ platforms/forge/src/main/java/com/pg85/otg/forge/commands/TPCommand.java
@@ -0,0 +1,112 @@
+package com.pg85.otg.forge.commands;
+
+import java.util.List;
+
+import com.pg85.otg.common.LocalWorld;
+import com.pg85.otg.forge.biomes.ForgeBiome;
+import com.pg85.otg.forge.dimensions.OTGTeleporter;
+import com.pg85.otg.util.ChunkCoordinate;
+
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.DimensionType;
+import net.minecraftforge.common.DimensionManager;
+
+public class TPCommand extends BaseCommand {
+	TPCommand() {
+
+		name = "tp";
+		usage = "tp <biome/dimension name or id>";
+		description = "Attempt to teleport to the target biome or dimension (Max range: 1000 chunks).";
+	}
+
+	@Override
+	public boolean onCommand(ICommandSender sender, List<String> args) {
+
+		if (args.isEmpty())
+			return false;
+
+		BlockPos pos = sender.getPosition();
+		int playerX = pos.getX();
+		int playerZ = pos.getZ();
+
+		LocalWorld world = this.getWorld(sender, "");
+
+		String biomeOrDimensionName = "";
+		for (int i = 0; i < args.size(); i++) {
+			biomeOrDimensionName += args.get(i) + " ";
+		}
+		if (biomeOrDimensionName != null && biomeOrDimensionName.trim().length() > 0) {
+			biomeOrDimensionName = biomeOrDimensionName.trim();
+			sender.sendMessage(
+					new TextComponentTranslation(MESSAGE_COLOR + "Searching for destination biome or dimension \""
+							+ VALUE_COLOR + biomeOrDimensionName + MESSAGE_COLOR + "\"."));
+
+			int biomeId = -1;
+			try {
+				biomeId = Integer.parseInt(biomeOrDimensionName.replace(" ", ""));
+			} catch (NumberFormatException ex) {
+			}
+
+			// Check dimension names
+			for (int i = -1; i < Long.SIZE << 4; i++) {
+				if (DimensionManager.isDimensionRegistered(i)) {
+					DimensionType dimensionType = DimensionManager.getProviderType(i);
+					if (dimensionType.getName().toLowerCase().trim().equals(biomeOrDimensionName.toLowerCase())) {
+						OTGTeleporter.changeDimension(i, (EntityPlayerMP) sender.getCommandSenderEntity(), false, true);
+						return true;
+					}
+				}
+			}
+
+			ChunkCoordinate playerChunk = ChunkCoordinate.fromBlockCoords(playerX, playerZ);
+			if (world != null) {
+				int maxRadius = 1000;
+
+				if (biomeId == -1) {
+					ForgeBiome targetBiome = (ForgeBiome) world.getBiomeByNameOrNull(biomeOrDimensionName);
+					if (targetBiome != null) {
+						biomeId = targetBiome.getIds().getOTGBiomeId();
+					}
+				}
+
+				if (biomeId != -1) {
+					for (int cycle = 1; cycle < maxRadius; cycle++) {
+						for (int x1 = playerX - cycle; x1 <= playerX + cycle; x1++) {
+							if (x1 == playerX - cycle || x1 == playerX + cycle) {
+								for (int z1 = playerZ - cycle; z1 <= playerZ + cycle; z1++) {
+									if (z1 == playerZ - cycle || z1 == playerZ + cycle) {
+										ChunkCoordinate chunkCoord = ChunkCoordinate.fromChunkCoords(
+												playerChunk.getChunkX() + (x1 - playerX),
+												playerChunk.getChunkZ() + (z1 - playerZ));
+
+										ForgeBiome biome = (ForgeBiome) world.getBiome(chunkCoord.getBlockXCenter(),
+												chunkCoord.getBlockZCenter());
+
+										if (biome != null && biome.getIds().getOTGBiomeId() == biomeId) {
+											sender.sendMessage(new TextComponentTranslation(
+													MESSAGE_COLOR + "Teleporting to \"" + VALUE_COLOR
+															+ biomeOrDimensionName + MESSAGE_COLOR + "\"."));
+											((Entity) sender).setPositionAndUpdate(chunkCoord.getBlockXCenter(),
+													world.getHighestBlockYAt(chunkCoord.getBlockXCenter(),
+															chunkCoord.getBlockZCenter(), true, true, false, false),
+													chunkCoord.getBlockZCenter());
+											return true;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			sender.sendMessage(new TextComponentTranslation(
+					ERROR_COLOR + "Could not find biome \"" + biomeOrDimensionName + "\"."));
+			return true;
+		}
+		return false;
+	}
+}
