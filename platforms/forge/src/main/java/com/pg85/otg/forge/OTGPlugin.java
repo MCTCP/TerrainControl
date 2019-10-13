4ee2e1eb287fa84e8894bc6f398772b45d59cdf1
PG85 <rik_lubking@hotmail.com>
2019-10-14 00:27:43 +0200

    v8_r8: UI fixes
    - Fixed the presets menu, cloning wasn't possible for presets that were in use, also no preset was selected when entering the menu, causing MC to crash when pressing Clone.

--- platforms/forge/src/main/java/com/pg85/otg/forge/OTGPlugin.java
+++ platforms/forge/src/main/java/com/pg85/otg/forge/OTGPlugin.java
@@ -43,7 +43,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
 //@Mod(modid = "openterraingenerator", name = "Open Terrain Generator", acceptableRemoteVersions = "*", version = "v2")
-@Mod(modid = "openterraingenerator", name = "Open Terrain Generator", version = "v8_r7", dependencies="required-after:otgcore@[1.12.2 - v8_r7]")
+@Mod(modid = "openterraingenerator", name = "Open Terrain Generator", version = "v8_r8", dependencies="required-after:otgcore@[1.12.2 - v8_r8]")
 public class OTGPlugin
 {	
 	@SidedProxy(clientSide="com.pg85.otg.forge.network.client.ClientProxy", serverSide="com.pg85.otg.forge.network.server.ServerProxy")
