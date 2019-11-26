d7959f1ef1c6b50701a69b06a58e8b560d21f0bb
PG85 <rik_lubking@hotmail.com>
2019-11-26 17:20:43 +0100

    v8.3_r4: Pregen, BO3Groups
    - Made sure all pregenerated chunks are populated
    - Fixed BO3Groups

--- platforms/forge/src/main/java/com/pg85/otg/forge/pregenerator/Pregenerator.java
+++ platforms/forge/src/main/java/com/pg85/otg/forge/pregenerator/Pregenerator.java
@@ -18,6 +18,7 @@ import com.pg85.otg.common.LocalWorld;
 import com.pg85.otg.configuration.dimensions.DimensionConfig;
 import com.pg85.otg.configuration.standard.WorldStandardValues;
 import com.pg85.otg.forge.ForgeWorld;
+import com.pg85.otg.generator.ChunkProviderOTG;
 import com.pg85.otg.logging.LogMarker;
 import com.pg85.otg.util.ChunkCoordinate;
 
@@ -392,25 +393,15 @@ public class Pregenerator
 		spawnedThisTick++;
 		updateProgressMessage(true);
 		
-		ChunkProviderServer chunkProvider = (ChunkProviderServer) world.getWorld().getChunkProvider();
-
-		Chunk chunk;
-        if (
-        	!(
-   				chunkProvider.chunkExists(currentX, currentZ) ||
-				RegionFileCache.createOrLoadRegionFile(((WorldServer)world.getWorld()).getChunkSaveLocation(), currentX, currentZ).chunkExists(currentX & 0x1F, currentZ & 0x1F)
-			)
-		)
-		{
-        	chunk = chunkProvider.provideChunk(currentX, currentZ);
-        	if(!chunk.isPopulated())
-        	{	        	
-	    		chunk.needsSaving(true);
-	        	chunkProvider.provideChunk(currentX, currentZ + 1).needsSaving(true);
-	        	chunkProvider.provideChunk(currentX + 1, currentZ).needsSaving(true);
-	        	chunkProvider.provideChunk(currentX + 1, currentZ + 1).needsSaving(true);	        	
-        	}
-		}       
+		ChunkProviderServer chunkProvider = (ChunkProviderServer) world.getWorld().getChunkProvider();		
+		Chunk chunk = chunkProvider.provideChunk(currentX, currentZ);
+    	if(!chunk.isPopulated())
+    	{	        	
+    		chunk.needsSaving(true);
+        	chunkProvider.provideChunk(currentX, currentZ + 1).needsSaving(true);
+        	chunkProvider.provideChunk(currentX + 1, currentZ).needsSaving(true);
+        	chunkProvider.provideChunk(currentX + 1, currentZ + 1).needsSaving(true);	        	
+    	}      
 		
 		if(spawned - lastSpawnedWhenSaved > compressCustomStructureCacheThreshHold)
 		{
