f38563172b73bcf47187769d8af68bd6c09f4418
PG85 <rik_lubking@hotmail.com>
2020-01-22 20:24:39 +0100

    9.0_r2: Fixed/redesigned /otg spawn (spaces)
    - /otg spawn now allows spaces and does not have a worldname param. OTG first searches the WorldObjects folder for the current world, then all dims, then the GlobalObjects folder.

--- platforms/forge/src/main/java/com/pg85/otg/forge/commands/SpawnCommand.java
+++ platforms/forge/src/main/java/com/pg85/otg/forge/commands/SpawnCommand.java
@@ -3,9 +3,12 @@ package com.pg85.otg.forge.commands;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.pg85.otg.OTG;
 import com.pg85.otg.common.LocalWorld;
 import com.pg85.otg.customobjects.CustomObject;
+import com.pg85.otg.forge.ForgeEngine;
 import com.pg85.otg.util.bo3.Rotation;
 
 import net.minecraft.command.ICommandSender;
@@ -23,7 +26,7 @@ public class SpawnCommand extends BaseCommand
     {
         super();
         name = "spawn";
-        usage = "spawn <object name> [world]";
+        usage = "spawn <object name>";
         description = "Spawn an object on the block you are looking at.";
     }
 
@@ -32,19 +35,39 @@ public class SpawnCommand extends BaseCommand
     {
         Random random = new Random();
 
-        LocalWorld forgeWorld = this.getWorld(sender, args.size() > 1 ? args.get(1) : "");
+        String objectName = StringUtils.join(args, " ").trim();
+        LocalWorld forgeWorld = ((ForgeEngine)OTG.getEngine()).getWorld(sender.getEntityWorld());
 
         if (args.isEmpty())
         {
             sender.sendMessage(new TextComponentString(ERROR_COLOR + "You must enter the name of the object."));
             return true;
         }
-        CustomObject spawnObject = null;
 
+        // Search the current world, if no BO3 is found, search dims, else globalobjects.
+        CustomObject spawnObject = null;
         if (forgeWorld != null)
         {
-            spawnObject = OTG.getCustomObjectManager().getGlobalObjects().getObjectByName(args.get(0),
-                    forgeWorld.getName());
+            spawnObject = OTG.getCustomObjectManager().getGlobalObjects().getObjectByName(objectName, forgeWorld.getName(), false);
+        }
+
+        if(spawnObject == null)
+        {
+	        for(LocalWorld localWorld : OTG.getAllWorlds())
+	        {
+	        	if(localWorld != forgeWorld)
+	        	{
+	        		spawnObject = OTG.getCustomObjectManager().getGlobalObjects().getObjectByName(objectName, localWorld.getName(), false);
+	        	}
+	        	if(spawnObject != null)
+	        	{
+	        		break;
+	        	}
+	        }
+        }
+        if(spawnObject == null)
+        {
+        	spawnObject = OTG.getCustomObjectManager().getGlobalObjects().getObjectByName(objectName, null, true);
         }
 
         if (spawnObject == null)
@@ -56,7 +79,6 @@ public class SpawnCommand extends BaseCommand
         RayTraceResult trace = this.rayTrace(sender.getEntityWorld(), (EntityPlayer) sender.getCommandSenderEntity());
         if (trace != null && trace.typeOfHit == RayTraceResult.Type.BLOCK)
         {
-
             BlockPos pos = trace.getBlockPos();
             if (spawnObject.spawnForced(forgeWorld, random, Rotation.NORTH, pos.getX(), pos.getY(), pos.getZ()))
             {
