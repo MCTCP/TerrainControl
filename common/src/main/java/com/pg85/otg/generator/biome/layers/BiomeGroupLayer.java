18ad72d6788c7ee1a9b058be7e39a1131eb31609
SuperCoder7979 <k.pranav@gmail.com>
2020-10-31 15:46:43 -0400

    1.16.2 - 0.0.1: Working biome group and group based biome generation

--- common/src/main/java/com/pg85/otg/generator/biome/layers/BiomeGroupLayer.java
+++ common/src/main/java/com/pg85/otg/generator/biome/layers/BiomeGroupLayer.java
@@ -3,24 +3,35 @@ package com.pg85.otg.generator.biome.layers;
 import static com.pg85.otg.generator.biome.layers.BiomeLayers.GROUP_SHIFT;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.pg85.otg.generator.biome.layers.type.ParentedLayer;
+import com.pg85.otg.generator.biome.layers.util.LayerRandomnessSource;
 import com.pg85.otg.generator.biome.layers.util.LayerSampleContext;
 import com.pg85.otg.generator.biome.layers.util.LayerSampler;
 
+/**
+ * Places a biome group at a certain depth.
+ */
 public class BiomeGroupLayer implements ParentedLayer
 {
-	private BiomePicker picker;
-
+	// The sorted map of rarities to biome groups
+	private final TreeMap<Integer, NewBiomeGroup> rarityMap = new TreeMap<>();
+	// The rarity sum of all the groups. This is used to choose the biome group.
+	private final int maxRarity;
 	public BiomeGroupLayer(List<NewBiomeGroup> groups) {
-		BiomePicker picker = new BiomePicker();
+		int maxRarity = 0;
 
+		// Iterate through groups and keep a tally of the rarity of each group.
+		// The order doesn't matter all that much, the margin between the values dictates the rarity.
 		for (NewBiomeGroup group : groups)
 		{
-			picker.addBiome(group.id, group.weight);
+			maxRarity += group.rarity;
+			this.rarityMap.put(maxRarity, group);
 		}
 
-		this.picker = picker;
+		this.maxRarity = maxRarity;
 	}
 
 	@Override
@@ -28,12 +39,32 @@ public class BiomeGroupLayer implements ParentedLayer
 	{
 		int sample = parent.sample(x, z);
 
-		if (BiomeLayers.isLand(sample))
+		// Check if it's land and then check if there is no group already here
+		if (BiomeLayers.isLand(sample) && BiomeLayers.getGroupId(sample) == 0)
 		{
-			int biomeGroup = this.picker.pickBiome(context);
+			int biomeGroup = getGroup(context);
+
+			// Encode the biome group id into the sample for later use
 			return sample | biomeGroup << GROUP_SHIFT;
 		}
 
 		return sample;
+	}
+
+	private int getGroup(LayerRandomnessSource random)
+	{
+		// Get a random rarity number from our max rarity
+		int chosenRarity = random.nextInt(maxRarity);
+
+		// Iterate through the rarity map and see if the chosen rarity is less than the rarity for each group, if it is then return.
+		for (Map.Entry<Integer, NewBiomeGroup> entry : rarityMap.entrySet())
+		{
+			if (chosenRarity < entry.getKey()) {
+				return entry.getValue().id;
+			}
+		}
+
+		// Fallback
+		return 0;
 	}
 }
--- common/src/main/java/com/pg85/otg/generator/biome/layers/NewBiomeGroup.java
+++ common/src/main/java/com/pg85/otg/generator/biome/layers/NewBiomeGroup.java
@@ -1,10 +1,16 @@
 package com.pg85.otg.generator.biome.layers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pg85.otg.generator.biome.NewBiomeData;
+
 /**
  * New biome group data for testing purposes
  */
 public class NewBiomeGroup
 {
 	public int id = 0;
-	public double weight = 0;
+	public int rarity = 0;
+	public List<NewBiomeData> biomes = new ArrayList<>();
 }
