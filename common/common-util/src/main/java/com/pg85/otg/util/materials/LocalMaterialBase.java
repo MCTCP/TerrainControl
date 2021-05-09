ec1e49efc6ffeac90c75b669b6485a10b9f57807
PG85 <rik_lubking@hotmail.com>
2021-05-10 00:55:46 +0200

    1.16.5 - 0.0.11: Fixed BlockTag writing to configs

--- common/common-util/src/main/java/com/pg85/otg/util/materials/LocalMaterialBase.java
+++ common/common-util/src/main/java/com/pg85/otg/util/materials/LocalMaterialBase.java
@@ -9,4 +9,6 @@ public abstract class LocalMaterialBase
 	// Used instead of instanceof to check between materials/tags
 	// TODO: Make this prettier, avoid requiring type checks altogether.
 	public abstract boolean isTag();
+	
+    public abstract String toString();
 }
