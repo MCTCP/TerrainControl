66768faeb7d6a096320a627ee2aed6fbca719a8c
PG85 <rik_lubking@hotmail.com>
2021-06-07 20:29:15 +0200

    1.16.5 - 0.0.12: Mixins instanceof, remove non-flush branch

--- platforms/forge/src/main/java/com/pg85/otg/forge/mixin/MixinChunkManager.java
+++ platforms/forge/src/main/java/com/pg85/otg/forge/mixin/MixinChunkManager.java
@@ -1,5 +1,9 @@
 package com.pg85.otg.forge.mixin;
 
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkPrimer;
+import net.minecraft.world.chunk.ChunkPrimerWrapper;
+import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.server.ChunkHolder;
 import net.minecraft.world.server.ChunkManager;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,14 +16,14 @@ import java.util.function.Predicate;
 public class MixinChunkManager
 {
     @ModifyArg(method = "saveAllChunks(Z)V", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", ordinal = 0))
-    private Predicate<ChunkHolder> alwaysAccessibleFlush(Predicate<ChunkHolder> chunkHolder)
+    private Predicate<ChunkHolder> alwaysAccessibleFlush(Predicate<ChunkHolder> chunk)
     {
         return c -> true;
-    }
-
-    @ModifyArg(method = "saveAllChunks(Z)V", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", ordinal = 3))
-    private Predicate<ChunkHolder> alwaysAccessible(Predicate<ChunkHolder> chunkHolder)
+    }	
+	
+    @ModifyArg(method = "saveAllChunks(Z)V", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", ordinal = 1))
+    private Predicate<IChunk> allowChunkPrimerFlush(Predicate<IChunk> chunk)
     {
-        return c -> true;
-    }
+        return c -> c instanceof ChunkPrimer || c instanceof ChunkPrimerWrapper || c instanceof Chunk;
+    }   
 }
