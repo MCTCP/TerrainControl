67f4a4a74e660f8ac0fcc07fb8ecd0ab94e89d2e
PG85 <rik_lubking@hotmail.com>
2021-07-03 13:09:55 +0200

    1.16.5-0.0.15: Multiple border biomes for the same biome at the same size

--- common/common-generator/src/main/java/com/pg85/otg/gen/biome/layers/BiomeBorderLayer.java
+++ common/common-generator/src/main/java/com/pg85/otg/gen/biome/layers/BiomeBorderLayer.java
@@ -1,5 +1,6 @@
 package com.pg85.otg.gen.biome.layers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,88 +10,144 @@ import com.pg85.otg.gen.biome.layers.util.LayerSampler;
 
 public class BiomeBorderLayer implements ParentedLayer
 { 
-	boolean[][] bordersFrom = new boolean[1024][];
-	private int[] bordersTo = new int[1024];
-	
+	private final BorderedBiome[] borderedBiomes;
+
 	public BiomeBorderLayer(BordersList borderBiomes)
 	{
-		this.bordersFrom = borderBiomes.bordersFrom;
-		this.bordersTo = borderBiomes.bordersTo;
+		this.borderedBiomes = borderBiomes.borderedBiomes;
+	}
+	
+	private static class BorderBiome
+	{
+		private final int id;
+		private final boolean[] allowedNearBiomes;
+		
+		private BorderBiome(int id, boolean[] allowedNearBiomes)
+		{
+			 this.id = id;
+			 this.allowedNearBiomes = allowedNearBiomes;
+		}
+	}
+	
+	private static class BorderedBiome
+	{
+		private final ArrayList<BorderBiome> biomeBorders = new ArrayList<>();	
 	}
 	
 	public static class BordersList
 	{
-		boolean[][] bordersFrom = new boolean[1024][];
-		private int[] bordersTo = new int[1024];
-						
-		public void addBorder(int biomeId, int biomeParent, List<Integer> notBorderNearBiomes)
+		private final BorderedBiome[] borderedBiomes = new BorderedBiome[1024]; 
+
+		public void addBorder(int biomeId, int borderedBiomeId, List<Integer> notBorderNearBiomes)
 		{
-			this.bordersFrom[biomeParent] = new boolean[1024];
-			Arrays.fill(this.bordersFrom[biomeParent], true);
+			BorderedBiome borderedBiome = this.borderedBiomes[borderedBiomeId];
+			if(borderedBiome == null)
+			{
+				borderedBiome = new BorderedBiome();
+				this.borderedBiomes[borderedBiomeId] = borderedBiome;
+			}
+			
+			boolean[] allowedNearBiomes = new boolean[1024];
+			Arrays.fill(allowedNearBiomes, true);
 			for (int notBorderNearBiome : notBorderNearBiomes)
 			{			
-				this.bordersFrom[biomeParent][notBorderNearBiome] = false;
-			}
-			this.bordersTo[biomeParent] = biomeId;
+				allowedNearBiomes[notBorderNearBiome] = false;
+			}			
+			BorderBiome borderBiome = new BorderBiome(biomeId, allowedNearBiomes);
+			
+			borderedBiome.biomeBorders.add(borderBiome);
 		}
 	}
-	
+
 	@Override
 	public int sample(LayerSampleContext<?> context, LayerSampler parent, int x, int z)
 	{
 		int center = parent.sample(x, z);
 		int cCheck = BiomeLayers.getBiomeFromLayer(center);
+		int northCheck;
+		int southCheck;
+		int eastCheck;
+		int westCheck;
+		int nwCheck;
+		int neCheck;
+		int swCheck;
+		int seCheck;
 		
-		if (bordersFrom[cCheck] != null)
+		// Check if there are border biomes for the biome in the center
+		if (this.borderedBiomes[cCheck] != null)
 		{
-			// Check in a + formation for edges
-			int northCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x, z - 1));
-			int southCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x + 1, z));
-			int eastCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x, z + 1));
-			int westCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x - 1, z));
-			boolean[] biomeFrom = bordersFrom[cCheck];
-			
-			if (biomeFrom[northCheck] && biomeFrom[eastCheck] && biomeFrom[westCheck] && biomeFrom[southCheck])
+			// For each bordered biome, loop through border biomes untill we can place one.
+			for(BorderBiome borderBiome : this.borderedBiomes[cCheck].biomeBorders)
 			{
-				if ((northCheck != cCheck) || (eastCheck != cCheck) || (westCheck != cCheck) || (southCheck != cCheck))
+				// Check in a + formation for edges
+				northCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x, z - 1));
+				southCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x + 1, z));
+				eastCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x, z + 1));
+				westCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x - 1, z));
+
+				if (
+					borderBiome.allowedNearBiomes[northCheck] && 
+					borderBiome.allowedNearBiomes[eastCheck] && 
+					borderBiome.allowedNearBiomes[westCheck] && 
+					borderBiome.allowedNearBiomes[southCheck]
+				)
 				{
-					// if it is suitable, set the edge biome
-					center = 
-						(
-						center & (
-							BiomeLayers.ISLAND_BIT
-							| BiomeLayers.RIVER_BITS
-							| BiomeLayers.ICE_BIT
-							)
-						)
-						| BiomeLayers.LAND_BIT
-						| bordersTo[cCheck]
-					;
-				} else {
-					// if it's not suitable, try again but sample in an X formation to make sure we didn't miss any potential edge
-					int nwCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x - 1, z - 1));
-					int neCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x + 1, z - 1));
-					int swCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x - 1, z + 1));
-					int seCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x + 1, z + 1));
-					
-					if (biomeFrom[nwCheck] && biomeFrom[neCheck] && biomeFrom[swCheck] && biomeFrom[seCheck])
+					// Check if there is a neighbouring biome.
+					if (
+						(northCheck != cCheck) || 
+						(eastCheck != cCheck) || 
+						(westCheck != cCheck) || 
+						(southCheck != cCheck)
+					)
 					{
-						if ((nwCheck != cCheck) || (neCheck != cCheck) || (swCheck != cCheck) || (seCheck != cCheck))
-						{
-							// if the second test is suitable, set the edge biome
-							center = 
-								(
-									center & (
-										BiomeLayers.ISLAND_BIT
-										| BiomeLayers.RIVER_BITS
-										| BiomeLayers.ICE_BIT
-									)
+						// Set the border biome
+						return 
+							(
+								center & (
+									BiomeLayers.ISLAND_BIT
+									| BiomeLayers.RIVER_BITS
+									| BiomeLayers.ICE_BIT
 								)
-								| BiomeLayers.LAND_BIT
-								| bordersTo[cCheck]
-							;
+							)
+							| BiomeLayers.LAND_BIT
+							| borderBiome.id
+						;
+					} else {
+						// if it's not suitable, try again but sample in an X formation to make sure we didn't miss any potential edge
+						nwCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x - 1, z - 1));
+						neCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x + 1, z - 1));
+						swCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x - 1, z + 1));
+						seCheck = BiomeLayers.getBiomeFromLayer(parent.sample(x + 1, z + 1));
+
+						if (
+							borderBiome.allowedNearBiomes[nwCheck] && 
+							borderBiome.allowedNearBiomes[neCheck] && 
+							borderBiome.allowedNearBiomes[swCheck] && 
+							borderBiome.allowedNearBiomes[seCheck]
+						)
+						{
+							// Check if there is a neighbouring biome.
+							if (
+								(nwCheck != cCheck) || 
+								(neCheck != cCheck) || 
+								(swCheck != cCheck) || 
+								(seCheck != cCheck)
+							)
+							{
+								// Set the border biome
+								return 
+									(
+										center & (
+											BiomeLayers.ISLAND_BIT
+											| BiomeLayers.RIVER_BITS
+											| BiomeLayers.ICE_BIT
+										)
+									)
+									| BiomeLayers.LAND_BIT
+									| borderBiome.id
+								;
+							}
 						}
-						// if the selection isn't suitable at this point, we can be almost certain that it's not an edge
 					}
 				}
 			}
