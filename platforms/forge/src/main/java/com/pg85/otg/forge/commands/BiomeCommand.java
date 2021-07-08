126a5b9effae9f1a9c9c2a218d08dc46127cfb58
PG85 <rik_lubking@hotmail.com>
2021-07-08 02:15:37 +0200

    1.16.5-0.0.15: OTG block tags for legacy configs, Forge.
    - Re-added old block tags like SAND, DIRT etc, to work as they did for 1.12.2. Forge only atm.

--- platforms/forge/src/main/java/com/pg85/otg/forge/commands/BiomeCommand.java
+++ platforms/forge/src/main/java/com/pg85/otg/forge/commands/BiomeCommand.java
@@ -1,6 +1,7 @@
 package com.pg85.otg.forge.commands;
 
 import com.pg85.otg.forge.gen.OTGNoiseChunkGenerator;
+
 import net.minecraft.command.CommandSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
@@ -14,12 +15,11 @@ public class BiomeCommand
 			source.sendSuccess(new StringTextComponent("OTG is not enabled in this world"), false);
 			return 0;
 		}
-		String MCBiome = source.getLevel().getBiome(
-				new BlockPos(source.getPosition().x, source.getPosition().y, source.getPosition().z)).getRegistryName().toString();
+
+		String MCBiome = source.getLevel().getBiome(new BlockPos(source.getPosition().x, source.getPosition().y, source.getPosition().z)).getRegistryName().toString();
 		String OTGBiome = ((OTGNoiseChunkGenerator)source.getLevel().getChunkSource().generator).getCachedBiomeProvider().getBiomeConfig((int)source.getPosition().x, (int)source.getPosition().z).getName();
-		
-		source.sendSuccess(new StringTextComponent("MC says: " + MCBiome + "\r\nOTG says:" + OTGBiome)
-			, false);
+
+		source.sendSuccess(new StringTextComponent("MC says: " + MCBiome + "\r\nOTG says:" + OTGBiome), false);
 		return 0;
 	}
 }
