3d4153cf687875a29a3a4e39b8ebe0a45b878b2d
coll1234567 <joshl5324@gmail.com>
2021-07-10 19:42:18 -0400

    Fix forge client server syncing

--- platforms/forge/src/main/java/com/pg85/otg/forge/network/BiomeSettingSyncWrapper.java
+++ platforms/forge/src/main/java/com/pg85/otg/forge/network/BiomeSettingSyncWrapper.java
@@ -8,7 +8,7 @@ import com.pg85.otg.util.biome.ColorSet;
 import com.pg85.otg.util.biome.ColorThreshold;
 import com.pg85.otg.util.biome.SimpleColorSet;
 
-import net.minecraft.network.PacketBuffer;
+import io.netty.buffer.ByteBuf;
 
 public class BiomeSettingSyncWrapper
 {
@@ -26,26 +26,30 @@ public class BiomeSettingSyncWrapper
 		this.waterColorControl = config.getWaterColorControl();
 	}
 
-	public BiomeSettingSyncWrapper(PacketBuffer buffer)
+	public BiomeSettingSyncWrapper(ByteBuf buffer)
 	{
 		this.fogDensity = buffer.readFloat();
+		byte size;
 
 		List<ColorThreshold> grassColors = new ArrayList<>();
-		for (int i = 0; i < buffer.readInt(); i++)
+		size = buffer.readByte();
+		for (int i = size; i > 0; i--)
 		{
 			grassColors.add(new ColorThreshold(buffer.readInt(), buffer.readFloat()));
 		}
 		this.grassColorControl = new SimpleColorSet(grassColors);
 
 		List<ColorThreshold> foliageColors = new ArrayList<>();
-		for (int i = 0; i < buffer.readInt(); i++)
+		size = buffer.readByte();
+		for (int i = size; i > 0; i--)
 		{
 			foliageColors.add(new ColorThreshold(buffer.readInt(), buffer.readFloat()));
 		}
 		this.foliageColorControl = new SimpleColorSet(foliageColors);
 
 		List<ColorThreshold> waterColors = new ArrayList<>();
-		for (int i = 0; i < buffer.readInt(); i++)
+		size = buffer.readByte();
+		for (int i = size; i > 0; i--)
 		{
 			waterColors.add(new ColorThreshold(buffer.readInt(), buffer.readFloat()));
 		}
@@ -73,25 +77,25 @@ public class BiomeSettingSyncWrapper
 		return waterColorControl;
 	}
 
-	public void encode(PacketBuffer buffer)
+	public void encode(ByteBuf buffer)
 	{
 		buffer.writeFloat(this.fogDensity);
 
-		buffer.writeByte((byte)this.grassColorControl.getLayers().size());
+		buffer.writeByte((byte) this.grassColorControl.getLayers().size());
 		for (ColorThreshold color : this.grassColorControl.getLayers())
 		{
 			buffer.writeInt(color.getColor());
 			buffer.writeFloat(color.getMaxNoise());
 		}
 
-		buffer.writeByte((byte)this.foliageColorControl.getLayers().size());
+		buffer.writeByte((byte) this.foliageColorControl.getLayers().size());
 		for (ColorThreshold color : this.foliageColorControl.getLayers())
 		{
 			buffer.writeInt(color.getColor());
 			buffer.writeFloat(color.getMaxNoise());
 		}
 
-		buffer.writeByte((byte)this.waterColorControl.getLayers().size());
+		buffer.writeByte((byte) this.waterColorControl.getLayers().size());
 		for (ColorThreshold color : this.waterColorControl.getLayers())
 		{
 			buffer.writeInt(color.getColor());
