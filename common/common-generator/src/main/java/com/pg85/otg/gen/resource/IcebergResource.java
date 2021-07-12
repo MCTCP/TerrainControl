a66119118ed730a8fad55f22c9011c7a73d160e4
PG85 <rik_lubking@hotmail.com>
2021-07-13 01:21:45 +0200

    Icebergs fix 2

--- common/common-generator/src/main/java/com/pg85/otg/gen/resource/IcebergResource.java
+++ common/common-generator/src/main/java/com/pg85/otg/gen/resource/IcebergResource.java
@@ -38,128 +38,121 @@ public class IcebergResource extends BiomeResourceBase implements IBasicResource
 		int x = world.getDecorationArea().getChunkBeingDecorated().getBlockX();
 		int z = world.getDecorationArea().getChunkBeingDecorated().getBlockZ();
 		int y = world.getBiomeConfigForDecoration(x, z).getWaterLevelMax();
-		boolean lvt_6_1_ = (random.nextDouble() > 0.7D);
+		boolean flag1 = random.nextDouble() > 0.7D;
 		LocalMaterialData iceBergMaterial = this.material;
+		double drandom1 = random.nextDouble() * 2.0D * Math.PI;
+		int irandom1 = 11 - random.nextInt(5);
+		int irandom2 = 3 + random.nextInt(3);
+		boolean flag2 = random.nextDouble() > 0.7D;
 
-		double lvt_8_1_ = random.nextDouble() * 2.0D * Math.PI;
-		int lvt_10_1_ = 11 - random.nextInt(5);
-		int lvt_11_1_ = 3 + random.nextInt(3);
-		boolean lvt_12_1_ = (random.nextDouble() > 0.7D);
-
-		int lvt_14_1_ = lvt_12_1_ ? (random.nextInt(6) + 6) : (random.nextInt(15) + 3);
-		if (!lvt_12_1_ && random.nextDouble() > 0.9D)
+		int ismoothheight = flag2 ? random.nextInt(6) + 6 : random.nextInt(15) + 3;
+		if (!flag2 && random.nextDouble() > 0.9D)
 		{
-			lvt_14_1_ += random.nextInt(19) + 7;
+			ismoothheight += random.nextInt(19) + 7;
 		}
 
-		int lvt_15_1_ = Math.min(lvt_14_1_ + random.nextInt(11), 18);
-		int lvt_16_1_ = Math.min(lvt_14_1_ + random.nextInt(7) - random.nextInt(5), 11);
-		int lvt_17_1_ = lvt_12_1_ ? lvt_10_1_ : 11;
+		int irandom4 = Math.min(ismoothheight + random.nextInt(11), 18);
+		int irandom5 = Math.min(ismoothheight + random.nextInt(7) - random.nextInt(5), 11);
+		int irandom6 = flag2 ? irandom1 : 11;
 
-		for (int lvt_18_1_ = -lvt_17_1_; lvt_18_1_ < lvt_17_1_; lvt_18_1_++)
+		for (int x1 = -irandom6; x1 < irandom6; x1++)
 		{
-			for (int lvt_19_1_ = -lvt_17_1_; lvt_19_1_ < lvt_17_1_; lvt_19_1_++)
+			for (int z1 = -irandom6; z1 < irandom6; z1++)
 			{
-				for (int lvt_20_1_ = 0; lvt_20_1_ < lvt_14_1_; lvt_20_1_++)
+				for (int y1 = 0; y1 < ismoothheight; y1++)
 				{
-					int lvt_21_1_ = lvt_12_1_ ? heightDependentRadiusEllipse(lvt_20_1_, lvt_14_1_, lvt_16_1_)
-							: heightDependentRadiusRound(random, lvt_20_1_, lvt_14_1_, lvt_16_1_);
-					if (lvt_12_1_ || lvt_18_1_ < lvt_21_1_)
+					int heightDependentRadiusEllipse = flag2 ? heightDependentRadiusEllipse(y1, ismoothheight, irandom5) : heightDependentRadiusRound(random, y1, ismoothheight, irandom5);
+					if (flag2 || x1 < heightDependentRadiusEllipse)
 					{
-						generateIcebergBlock(world, random, x, y, z, lvt_14_1_, lvt_18_1_, lvt_20_1_, lvt_19_1_,
-								lvt_21_1_, lvt_17_1_, lvt_12_1_, lvt_11_1_, lvt_8_1_, lvt_6_1_, iceBergMaterial);
+						generateIcebergBlock(world, random, x, y, z, ismoothheight, x1, y1, z1, heightDependentRadiusEllipse, irandom6, flag2, irandom2, drandom1, flag1, iceBergMaterial);
 					}
 				}
 			}
 		}
-		smooth(world, x, y, z, lvt_16_1_, lvt_14_1_, lvt_12_1_, lvt_10_1_);
 
-		for (int lvt_18_2_ = -lvt_17_1_; lvt_18_2_ < lvt_17_1_; lvt_18_2_++)
+		smooth(world, x, y, z, irandom5, ismoothheight, flag2, irandom1);
+
+		for (int x1 = -irandom6; x1 < irandom6; x1++)
 		{
-			for (int lvt_19_2_ = -lvt_17_1_; lvt_19_2_ < lvt_17_1_; lvt_19_2_++)
+			for (int z1 = -irandom6; z1 < irandom6; z1++)
 			{
-				for (int lvt_20_2_ = -1; lvt_20_2_ > -lvt_15_1_; lvt_20_2_--)
+				for (int y1 = -1; y1 > -irandom4; y1--)
 				{
-					int lvt_21_2_ = lvt_12_1_
-							? MathHelper.ceil(lvt_17_1_ * (1.0F - (float) Math.pow(lvt_20_2_, 2.0D) / lvt_15_1_ * 8.0F))
-							: lvt_17_1_;
-					int lvt_22_1_ = heightDependentRadiusSteep(random, -lvt_20_2_, lvt_15_1_, lvt_16_1_);
-					if (lvt_18_2_ < lvt_22_1_)
+					int irandom7 = flag2 ? MathHelper.ceil((float)irandom6 * (1.0F - (float) Math.pow((double)y1, 2.0D) / ((float)irandom4 * 8.0F))) : irandom6;
+					int heightDependentRadiusSteep = heightDependentRadiusSteep(random, -y1, irandom4, irandom5);
+					if (x1 < heightDependentRadiusSteep)
 					{
-						generateIcebergBlock(world, random, x, y, z, lvt_15_1_, lvt_18_2_, lvt_20_2_, lvt_19_2_,
-								lvt_22_1_, lvt_21_2_, lvt_12_1_, lvt_11_1_, lvt_8_1_, lvt_6_1_, iceBergMaterial);
+						generateIcebergBlock(world, random, x, y, z, irandom4, x1, y1, z1, heightDependentRadiusSteep, irandom7, flag2, irandom2, drandom1, flag1, iceBergMaterial);
 					}
 				}
 			}
 		}
-		boolean lvt_18_3_ = lvt_12_1_ ? ((random.nextDouble() > 0.1D)) : ((random.nextDouble() > 0.7D));
-		if (lvt_18_3_)
+		boolean flag3 = flag2 ? random.nextDouble() > 0.1D : random.nextDouble() > 0.7D;
+		if (flag3)
 		{
-			generateCutOut(random, world, lvt_16_1_, lvt_14_1_, x, y, z, lvt_12_1_, lvt_10_1_, lvt_8_1_, lvt_11_1_);
+			generateCutOut(random, world, irandom5, ismoothheight, x, y, z, flag2, irandom1, drandom1, irandom2);
 		}
 	}
 
-	private void generateCutOut(Random random, IWorldGenRegion world, int p_205184_3_, int p_205184_4_,
-			int x, int y, int z, boolean p_205184_6_, int p_205184_7_, double p_205184_8_, int p_205184_10_)
+	private void generateCutOut(Random random, IWorldGenRegion world, int irandom, int irandom1, int x, int y, int z, boolean flag, int irandom2, double drandom1, int irandom3)
 	{
-		int lvt_11_1_ = random.nextBoolean() ? -1 : 1;
-		int lvt_12_1_ = random.nextBoolean() ? -1 : 1;
-
-		int lvt_13_1_ = random.nextInt(Math.max(p_205184_3_ / 2 - 2, 1));
+		int irandom4 = random.nextBoolean() ? -1 : 1;
+		int irandom5 = random.nextBoolean() ? -1 : 1;
+		int irandom6 = random.nextInt(Math.max(irandom / 2 - 2, 1));
 		if (random.nextBoolean())
 		{
-			lvt_13_1_ = p_205184_3_ / 2 + 1 - random.nextInt(Math.max(p_205184_3_ - p_205184_3_ / 2 - 1, 1));
+			irandom6 = irandom / 2 + 1 - random.nextInt(Math.max(irandom - irandom / 2 - 1, 1));
 		}
 
-		int lvt_14_1_ = random.nextInt(Math.max(p_205184_3_ / 2 - 2, 1));
+		int irandom7 = random.nextInt(Math.max(irandom / 2 - 2, 1));
 		if (random.nextBoolean())
 		{
-			lvt_14_1_ = p_205184_3_ / 2 + 1 - random.nextInt(Math.max(p_205184_3_ - p_205184_3_ / 2 - 1, 1));
+			irandom7 = irandom / 2 + 1 - random.nextInt(Math.max(irandom - irandom / 2 - 1, 1));
 		}
 
-		if (p_205184_6_)
+		if (flag)
 		{
-			lvt_13_1_ = lvt_14_1_ = random.nextInt(Math.max(p_205184_7_ - 5, 1));
+			irandom6 = irandom7 = random.nextInt(Math.max(irandom2 - 5, 1));
 		}
 
-		int x2 = lvt_11_1_ * lvt_13_1_;
-		int z2 = lvt_12_1_ * lvt_14_1_;
-		double lvt_16_1_ = p_205184_6_ ? (p_205184_8_ + 1.5707963267948966D) : (random.nextDouble() * 2.0D * Math.PI);
+		int x2 = irandom4 * irandom6;
+		int z2 = irandom5 * irandom7;
+		double drandom2 = flag ? drandom1 + (Math.PI / 2D) : random.nextDouble() * 2.0D * Math.PI;
+		//double drandom2 = flag ? (drandom1 + 1.5707963267948966D) : (random.nextDouble() * 2.0D * Math.PI);
 
-		for (int lvt_18_1_ = 0; lvt_18_1_ < p_205184_4_ - 3; ++lvt_18_1_)
+		for (int irandom8 = 0; irandom8 < irandom1 - 3; ++irandom8)
 		{
-			int lvt_19_1_ = heightDependentRadiusRound(random, lvt_18_1_, p_205184_4_, p_205184_3_);
-			carve(lvt_19_1_, lvt_18_1_, x, y, z, world, false, lvt_16_1_, x2, z2, p_205184_7_, p_205184_10_);
-
+			int heightDependentRadiusRound = heightDependentRadiusRound(random, irandom8, irandom1, irandom);
+			carve(heightDependentRadiusRound, irandom8, x, y, z, world, false, drandom2, x2, z2, irandom2, irandom3);
 		}
-		for (int lvt_18_2_ = -1; lvt_18_2_ > -p_205184_4_ + random.nextInt(5); --lvt_18_2_)
+		for (int irandom8 = -1; irandom8 > -irandom1 + random.nextInt(5); --irandom8)
 		{
-			int lvt_19_2_ = heightDependentRadiusSteep(random, -lvt_18_2_, p_205184_4_, p_205184_3_);
-			carve(lvt_19_2_, lvt_18_2_, x, y, z, world, true, lvt_16_1_, x2, z2, p_205184_7_, p_205184_10_);
+			int heightDependentRadiusSteep = heightDependentRadiusSteep(random, -irandom8, irandom1, irandom);
+			carve(heightDependentRadiusSteep, irandom8, x, y, z, world, true, drandom2, x2, z2, irandom2, irandom3);
 		}
 	}
 
-	private void carve(int p_205174_1_, int p_205174_2_, int x, int y, int z, IWorldGenRegion world, boolean p_205174_5_, double p_205174_6_, int x3, int z3, int p_205174_9_, int p_205174_10_)
+	private void carve(int heightDependentRadius, int irandom, int x, int y, int z, IWorldGenRegion world, boolean flag, double drandom, int x3, int z3, int irandom1, int irandom2)
 	{
-		int lvt_11_1_ = p_205174_1_ + 1 + p_205174_9_ / 3;
-		int lvt_12_1_ = Math.min(p_205174_1_ - 3, 3) + p_205174_10_ / 2 - 1;
+		int irandom3 = heightDependentRadius + 1 + irandom1 / 3;
+		int irandom4 = Math.min(heightDependentRadius - 3, 3) + irandom2 / 2 - 1;
 		int x2;
 		int y2;
 		int z2;
-		for (int lvt_13_1_ = -lvt_11_1_; lvt_13_1_ < lvt_11_1_; lvt_13_1_++)
+		for (int x1 = -irandom3; x1 < irandom3; x1++)
 		{
-			for (int lvt_14_1_ = -lvt_11_1_; lvt_14_1_ < lvt_11_1_; lvt_14_1_++)
+			for (int z1 = -irandom3; z1 < irandom3; z1++)
 			{
-				double lvt_15_1_ = signedDistanceEllipse(lvt_13_1_, lvt_14_1_, x3, z3, lvt_11_1_, lvt_12_1_, p_205174_6_);
-				if (lvt_15_1_ < 0.0D)
+				double signedDistanceEllipse = signedDistanceEllipse(x1, z1, x3, 0, z3, irandom3, irandom4, drandom);
+				if (signedDistanceEllipse < 0.0D)
 				{
-					x2 = x + lvt_13_1_;
-					y2 = y + p_205174_2_;
-					z2 = z + lvt_14_1_;
+					x2 = x + x1;
+					y2 = y + irandom;
+					z2 = z + z1;
 					LocalMaterialData material = world.getMaterialDirect(x2, y2, z2);
 					if (isIcebergBlock(material) || material.isMaterial(LocalMaterials.SNOW_BLOCK))
 					{
-						if (p_205174_5_)
+						if (flag)
 						{
 							world.setBlockDirect(x2, y2, z2, LocalMaterials.WATER);
 						} else {
@@ -181,46 +174,34 @@ public class IcebergResource extends BiomeResourceBase implements IBasicResource
 		}
 	}
 
-	private void generateIcebergBlock(
-		IWorldGenRegion p_205181_1_, Random p_205181_2_, int x, int y, int z, int p_205181_4_, 
-		int p_205181_5_, int p_205181_6_, int p_205181_7_, int p_205181_8_, int p_205181_9_, 
-		boolean p_205181_10_, int p_205181_11_, double p_205181_12_, boolean p_205181_14_, 
-		LocalMaterialData material
-	)
+	private void generateIcebergBlock(IWorldGenRegion worldGenRegion, Random random, int x, int y, int z, int irandom, int x1, int y1, int z1, int irandom1, int irandom2, boolean flag, int irandom3, double drandom, boolean flag1, LocalMaterialData material)
 	{
-		double lvt_16_1_ = p_205181_10_
-			? signedDistanceEllipse(p_205181_5_, p_205181_7_, 0, 0, p_205181_9_, getEllipseC(p_205181_6_, p_205181_4_, p_205181_11_), p_205181_12_)
-			: signedDistanceCircle(p_205181_5_, p_205181_7_, 0, 0, p_205181_8_, p_205181_2_);
+		double signedDistance = flag
+			? signedDistanceEllipse(x1, z1, 0, 0, 0, irandom2, getEllipseC(y1, irandom, irandom3), drandom)
+			: signedDistanceCircle(x1, z1, 0, 0, 0, irandom1, random);
 
-		if (lvt_16_1_ < 0.0D)
+		if (signedDistance < 0.0D)
 		{
-			int x2 = x + p_205181_5_;
-			int y2 = y + p_205181_6_;
-			int z2 = z + p_205181_7_;
-			double lvt_19_1_ = p_205181_10_ ? -0.5D : (-6 - p_205181_2_.nextInt(3));
-			if (lvt_16_1_ > lvt_19_1_ && p_205181_2_.nextDouble() > 0.9D)
+			int x2 = x + x1;
+			int y2 = y + y1;
+			int z2 = z + z1;
+			double lvt_19_1_ = flag ? -0.5D : (double)(-6 - random.nextInt(3));
+			if (signedDistance > lvt_19_1_ && random.nextDouble() > 0.9D)
 			{
 				return;
 			}
-			setIcebergBlock(x2, y2, z2, p_205181_1_, p_205181_2_, p_205181_4_ - p_205181_6_, p_205181_4_, p_205181_10_, p_205181_14_, material);
+			setIcebergBlock(x2, y2, z2, worldGenRegion, random, irandom - y1, irandom, flag, flag1, material);
 		}
 	}
 
-	private void setIcebergBlock(
-		int x, int y, int z, IWorldGenRegion world, Random p_205175_3_, int p_205175_4_,
-		int p_205175_5_, boolean p_205175_6_, boolean p_205175_7_, LocalMaterialData material
-	)
+	private void setIcebergBlock(int x, int y, int z, IWorldGenRegion world, Random random, int irandomy, int irandom, boolean flag, boolean flag1, LocalMaterialData material)
 	{
 		LocalMaterialData current = world.getMaterialDirect(x, y, z);
 		if (current.isAir() || current.isMaterial(LocalMaterials.SNOW_BLOCK) || current.isMaterial(LocalMaterials.ICE) || current.isMaterial(LocalMaterials.WATER))
 		{
-			boolean lvt_10_1_ = (!p_205175_6_ || p_205175_3_.nextDouble() > 0.05D);
-			int lvt_11_1_ = p_205175_6_ ? 3 : 2;
-			if (
-				p_205175_7_ && (current == null || !current.isMaterial(LocalMaterials.WATER))
-				&& p_205175_4_ <= p_205175_3_.nextInt(Math.max(1, p_205175_5_ / lvt_11_1_)) + p_205175_5_ * 0.6D
-				&& lvt_10_1_
-			)
+			boolean flag2 = !flag || random.nextDouble() > 0.05D;
+			int irandom1 = flag ? 3 : 2;
+			if (flag1 && !current.isMaterial(LocalMaterials.WATER) && (double)irandomy <= (double)random.nextInt(Math.max(1, irandom / irandom1)) + (double)irandom * 0.6D && flag2)
 			{
 				world.setBlockDirect(x, y, z, LocalMaterials.SNOW_BLOCK);
 			} else {
@@ -229,64 +210,57 @@ public class IcebergResource extends BiomeResourceBase implements IBasicResource
 		}
 	}
 
-	private int getEllipseC(int p_205176_1_, int p_205176_2_, int p_205176_3_)
+	private int getEllipseC(int y1, int irandom1, int irandom2)
 	{
-		int lvt_4_1_ = p_205176_3_;
-		if (p_205176_1_ > 0 && p_205176_2_ - p_205176_1_ <= 3)
+		int ellipsec = irandom2;
+		if (y1 > 0 && irandom1 - y1 <= 3)
 		{
-			lvt_4_1_ -= 4 - p_205176_2_ - p_205176_1_;
+			ellipsec -= (4 - (irandom1 - y1));
 		}
 
-		return lvt_4_1_;
+		return ellipsec;
 	}
 
-	private double signedDistanceCircle(int p_205177_1_, int p_205177_2_, int x, int z, int p_205177_4_, Random random)
+	private double signedDistanceCircle(int x1, int z1, int x, int y, int z, int irandom, Random random)
 	{
-		float lvt_6_1_ = (float) (10.0F * MathHelper.clamp(random.nextFloat(), 0.2F, 0.8F) / p_205177_4_);
-		return lvt_6_1_ + Math.pow((p_205177_1_ - x), 2.0D) + Math.pow((p_205177_2_ - z), 2.0D) - Math.pow(p_205177_4_, 2.0D);
+		float frandom = 10.0F * MathHelper.clamp(random.nextFloat(), 0.2F, 0.8F) / (float)irandom;
+		return (double)frandom + Math.pow((double)(x1 - x), 2.0D) + Math.pow((double)(z1 - z), 2.0D) - Math.pow((double)irandom, 2.0D);
 	}
 
-	private double signedDistanceEllipse(int p_205180_1_, int p_205180_2_, int x, int z, int p_205180_4_, int p_205180_5_, double p_205180_6_)
+	private double signedDistanceEllipse(int x1, int z1, int x, int y, int z, int irandom, int iellipsec, double drandom)
 	{
-		return Math
-			.pow(((p_205180_1_ - x) * Math.cos(p_205180_6_) - (p_205180_2_ - z) * Math.sin(p_205180_6_))
-					/ p_205180_4_, 2.0D)
-			+ Math.pow(((p_205180_1_ - x) * Math.sin(p_205180_6_) + (p_205180_2_ - z) * Math.cos(p_205180_6_))
-					/ p_205180_5_, 2.0D)
-			- 1.0D;
+		return Math.pow(((double)(x1 - x) * Math.cos(drandom) - (double)(z1 - z) * Math.sin(drandom)) / (double)irandom, 2.0D) + Math.pow(((double)(x1 - x) * Math.sin(drandom) + (double)(z1 - z) * Math.cos(drandom)) / (double)iellipsec, 2.0D) - 1.0D;
 	}
 
-	private int heightDependentRadiusRound(Random p_205183_1_, int p_205183_2_, int p_205183_3_, int p_205183_4_)
+	private int heightDependentRadiusRound(Random random, int irandom1, int irandom2, int irandom3)
 	{
-		float lvt_5_1_ = 3.5F - p_205183_1_.nextFloat();
-		float lvt_6_1_ = (1.0F - (float) Math.pow(p_205183_2_, 2.0D) / p_205183_3_ * lvt_5_1_) * p_205183_4_;
-
-		if (p_205183_3_ > 15 + p_205183_1_.nextInt(5))
+		float frandom1 = 3.5F - random.nextFloat();
+		float frandom2 = (1.0F - (float) Math.pow((double)irandom1, 2.0D) / ((float)irandom2 * frandom1)) * (float)irandom3;
+		if (irandom2 > 15 + random.nextInt(5))
 		{
-			int lvt_7_1_ = (p_205183_2_ < 3 + p_205183_1_.nextInt(6)) ? (p_205183_2_ / 2) : p_205183_2_;
-			lvt_6_1_ = (1.0F - lvt_7_1_ / p_205183_3_ * lvt_5_1_ * 0.4F) * p_205183_4_;
-
+			int irandom4 = irandom1 < 3 + random.nextInt(6) ? irandom1 / 2 : irandom1;
+			frandom2 = (1.0F - (float)irandom4 / ((float)irandom2 * frandom1 * 0.4F)) * (float)irandom3;
 		}
-		return MathHelper.ceil(lvt_6_1_ / 2.0F);
+		return MathHelper.ceil(frandom2 / 2.0F);
 	}
 
-	private int heightDependentRadiusEllipse(int p_205178_1_, int p_205178_2_, int p_205178_3_)
+	private int heightDependentRadiusEllipse(int irandomy, int irandom1, int irandom2)
 	{
-		float lvt_4_1_ = 1.0F;
-		float lvt_5_1_ = (1.0F - (float) Math.pow(p_205178_1_, 2.0D) / p_205178_2_ * 1.0F) * p_205178_3_;
-		return MathHelper.ceil(lvt_5_1_ / 2.0F);
+		float f1 = 1.0F;
+		float frandom = (1.0F - (float)Math.pow((double)irandomy, 2.0D) / ((float)irandom1 * 1.0F)) * (float)irandom2;
+		return MathHelper.ceil(frandom / 2.0F);
 	}
 
-	private int heightDependentRadiusSteep(Random p_205187_1_, int p_205187_2_, int p_205187_3_, int p_205187_4_)
+	private int heightDependentRadiusSteep(Random random, int random1, int random2, int random3)
 	{
-		float lvt_5_1_ = 1.0F + p_205187_1_.nextFloat() / 2.0F;
-		float lvt_6_1_ = (1.0F - p_205187_2_ / p_205187_3_ * lvt_5_1_) * p_205187_4_;
-		return MathHelper.ceil(lvt_6_1_ / 2.0F);
+		float frandom = 1.0F + random.nextFloat() / 2.0F;
+		float frandom1 = (1.0F - (float)random1 / ((float)random2 * frandom)) * (float)random3;
+		return MathHelper.ceil(frandom1 / 2.0F);
 	}
 
 	private boolean isIcebergBlock(LocalMaterialData material)
 	{
-		return (material.isMaterial(LocalMaterials.PACKED_ICE) || material.isMaterial(LocalMaterials.SNOW_BLOCK) || material.isMaterial(LocalMaterials.BLUE_ICE));
+		return material.isMaterial(LocalMaterials.PACKED_ICE) || material.isMaterial(LocalMaterials.SNOW_BLOCK) || material.isMaterial(LocalMaterials.BLUE_ICE);
 	}
 
 	private boolean belowIsAir(IWorldGenRegion world, int x, int y, int z)
@@ -294,22 +268,22 @@ public class IcebergResource extends BiomeResourceBase implements IBasicResource
 		return world.getMaterialDirect(x, y - 1, z).isAir();
 	}
 
-	private void smooth(IWorldGenRegion world, int x, int y, int z, int p_205186_3_, int p_205186_4_, boolean p_205186_5_, int p_205186_6_)
+	private void smooth(IWorldGenRegion world, int x, int y, int z, int irandom1, int ismoothheight, boolean flag, int irandom3)
 	{
-		int lvt_7_1_ = p_205186_5_ ? p_205186_6_ : (p_205186_3_ / 2);
+		int iradius = flag ? irandom3 : irandom1 / 2;
 
 		int x2;
 		int y2;
 		int z2;
-		for (int lvt_8_1_ = -lvt_7_1_; lvt_8_1_ <= lvt_7_1_; lvt_8_1_++)
+		for (int x1 = -iradius; x1 <= iradius; x1++)
 		{
-			for (int lvt_9_1_ = -lvt_7_1_; lvt_9_1_ <= lvt_7_1_; lvt_9_1_++)
+			for (int z1 = -iradius; z1 <= iradius; z1++)
 			{
-				for (int lvt_10_1_ = 0; lvt_10_1_ <= p_205186_4_; lvt_10_1_++)
+				for (int y1 = 0; y1 <= ismoothheight; y1++)
 				{
-					x2 = x + lvt_8_1_;
-					y2 = y + lvt_10_1_;
-					z2 = z + lvt_9_1_;
+					x2 = x + x1;
+					y2 = y + y1;
+					z2 = z + z1;
 					LocalMaterialData material = world.getMaterialDirect(x2, y2, z2);
 					if (isIcebergBlock(material) || material.isMaterial(LocalMaterials.SNOW))
 					{
@@ -319,21 +293,23 @@ public class IcebergResource extends BiomeResourceBase implements IBasicResource
 							world.setBlockDirect(x2, y2 + 1, z2, LocalMaterials.AIR);	
 						}
 						else if (isIcebergBlock(material))
-						{	
-							LocalMaterialData[] lvt_13_1_ =
+						{
+							LocalMaterialData[] materials =
 							{
-								world.getMaterialDirect(x2 + 1, y2, z2), world.getMaterialDirect(x2 - 1, y2, z2),
-								world.getMaterialDirect(x2, y2, z2 + 1), world.getMaterialDirect(x2, y2, z2 - 1) 
+								world.getMaterialDirect(x2 - 1, y2, z2), 
+								world.getMaterialDirect(x2 + 1, y2, z2),
+								world.getMaterialDirect(x2, y2, z2 - 1), 
+								world.getMaterialDirect(x2, y2, z2 + 1) 
 							};
-							int lvt_14_1_ = 0;
-							for (LocalMaterialData mat : lvt_13_1_)
+							int iheight = 0;
+							for (LocalMaterialData mat : materials)
 							{
 								if (!isIcebergBlock(mat))
 								{
-									lvt_14_1_++;
+									iheight++;
 								}
 							}
-							if (lvt_14_1_ >= 3)
+							if (iheight >= 3)
 							{
 								world.setBlockDirect(x2, y2, z2, LocalMaterials.AIR);
 							}
