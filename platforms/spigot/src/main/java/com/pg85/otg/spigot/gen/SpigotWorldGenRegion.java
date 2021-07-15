2ef2654044e8ade3167c690ade0a0906307f5441
PG85 <rik_lubking@hotmail.com>
2021-07-15 10:23:55 +0200

    1.16.5-0.0.16: Iceberg() uses replacedblocks.

--- platforms/spigot/src/main/java/com/pg85/otg/spigot/gen/SpigotWorldGenRegion.java
+++ platforms/spigot/src/main/java/com/pg85/otg/spigot/gen/SpigotWorldGenRegion.java
@@ -310,12 +310,20 @@ public class SpigotWorldGenRegion extends LocalWorldGenRegion
 		return -1;
 	}
 
+	// Only used by new 1.16 resources that use the 3x3 decoration area.
+	// Once we've updated to 3x3, should remove this and use 
+	// decorationBiomeCache and decorationArea for everything.
 	@Override
 	public void setBlockDirect(int x, int y, int z, LocalMaterialData material)
 	{
+		IBiomeConfig biomeConfig = this.getCachedBiomeProvider().getBiomeConfig(x, z, true);
+		if(biomeConfig.getReplaceBlocks() != null)
+		{
+			material = material.parseWithBiomeAndHeight(this.getWorldConfig().getBiomeConfigsHaveReplacement(), biomeConfig.getReplaceBlocks(), y);
+		}
 		this.worldGenRegion.setTypeAndData(new BlockPosition(x, y, z), ((SpigotMaterialData)material).internalBlock(), 3);
 	}
-	
+
 	@Override
 	public void setBlock(int x, int y, int z, LocalMaterialData material)
 	{
