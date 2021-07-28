37b4c803c6c81776a2e3e901f89e334022d148ef
PG85 <rik_lubking@hotmail.com>
2021-07-28 15:55:34 +0200

    1.16.5-0.0.17: Added legacy double plant aliases for Plant/UnderWaterPlant/Grass/SurfacePatch

--- common/common-util/src/main/java/com/pg85/otg/util/minecraft/PlantType.java
+++ common/common-util/src/main/java/com/pg85/otg/util/minecraft/PlantType.java
@@ -18,6 +18,7 @@ public class PlantType
 {
 	// Builds lookup map
 	private static final Map<String, PlantType> LOOKUP_MAP = new TreeMap<String, PlantType>(String.CASE_INSENSITIVE_ORDER);
+	private static final Map<String, PlantType> ALIAS_LOOKUP_MAP = new TreeMap<String, PlantType>(String.CASE_INSENSITIVE_ORDER);
 
 	public static final PlantType SeaGrass = register(new PlantType("SeaGrass", LocalMaterials.SEAGRASS));
 	public static final PlantType Allium = register(new PlantType("Allium", LocalMaterials.ALLIUM));
@@ -27,19 +28,19 @@ public class PlantType
 	public static final PlantType Dandelion = register(new PlantType("Dandelion", LocalMaterials.YELLOW_FLOWER));
 	public static final PlantType DeadBush = register(new PlantType("DeadBush", LocalMaterials.DEAD_BUSH));
 	public static final PlantType DoubleTallSeaGrass = register(new PlantType("DoubleTallSeaGrass", LocalMaterials.TALL_SEAGRASS_LOWER, LocalMaterials.TALL_SEAGRASS_UPPER));	
-	public static final PlantType DoubleTallGrass = register(new PlantType("DoubleTallgrass", LocalMaterials.DOUBLE_TALL_GRASS_LOWER, LocalMaterials.DOUBLE_TALL_GRASS_UPPER));
+	public static final PlantType DoubleTallGrass = register(new PlantType("DoubleTallgrass", new String[] { "minecraft:double_plant:2", "double_plant:2" }, LocalMaterials.DOUBLE_TALL_GRASS_LOWER, LocalMaterials.DOUBLE_TALL_GRASS_UPPER));
 	public static final PlantType Fern = register(new PlantType("Fern", LocalMaterials.FERN));
-	public static final PlantType LargeFern = register(new PlantType("LargeFern", LocalMaterials.LARGE_FERN_LOWER, LocalMaterials.LARGE_FERN_UPPER));
-	public static final PlantType Lilac = register(new PlantType("Lilac", LocalMaterials.LILAC_LOWER, LocalMaterials.LILAC_UPPER));
+	public static final PlantType LargeFern = register(new PlantType("LargeFern", new String[] { "minecraft:double_plant:3", "double_plant:3" }, LocalMaterials.LARGE_FERN_LOWER, LocalMaterials.LARGE_FERN_UPPER));
+	public static final PlantType Lilac = register(new PlantType("Lilac", new String[] { "minecraft:double_plant:1", "double_plant:1" }, LocalMaterials.LILAC_LOWER, LocalMaterials.LILAC_UPPER));
 	public static final PlantType OrangeTulip = register(new PlantType("OrangeTulip", LocalMaterials.ORANGE_TULIP));
 	public static final PlantType OxeyeDaisy = register(new PlantType("OxeyeDaisy", LocalMaterials.OXEYE_DAISY));
-	public static final PlantType Peony = register(new PlantType("Peony", LocalMaterials.PEONY_LOWER, LocalMaterials.PEONY_UPPER));
+	public static final PlantType Peony = register(new PlantType("Peony", new String[] { "minecraft:double_plant:5", "double_plant:5" }, LocalMaterials.PEONY_LOWER, LocalMaterials.PEONY_UPPER));
 	public static final PlantType PinkTulip = register(new PlantType("PinkTulip", LocalMaterials.PINK_TULIP));
 	public static final PlantType Poppy = register(new PlantType("Poppy", LocalMaterials.POPPY));
 	public static final PlantType RedMushroom = register(new PlantType("RedMushroom", LocalMaterials.RED_MUSHROOM));
 	public static final PlantType RedTulip = register(new PlantType("RedTulip", LocalMaterials.RED_TULIP));
-	public static final PlantType RoseBush = register(new PlantType("RoseBush", LocalMaterials.ROSE_BUSH_LOWER, LocalMaterials.ROSE_BUSH_UPPER));
-	public static final PlantType Sunflower = register(new PlantType("Sunflower", LocalMaterials.SUNFLOWER_LOWER, LocalMaterials.SUNFLOWER_UPPER));
+	public static final PlantType RoseBush = register(new PlantType("RoseBush", new String[] { "minecraft:double_plant:4", "double_plant:4" }, LocalMaterials.ROSE_BUSH_LOWER, LocalMaterials.ROSE_BUSH_UPPER));
+	public static final PlantType Sunflower = register(new PlantType("Sunflower", new String[] { "minecraft:double_plant", "double_plant", "minecraft:double_plant:0", "double_plant:0" }, LocalMaterials.SUNFLOWER_LOWER, LocalMaterials.SUNFLOWER_UPPER));
 	public static final PlantType Tallgrass = register(new PlantType("Tallgrass", LocalMaterials.LONG_GRASS));
 	public static final PlantType WhiteTulip = register(new PlantType("WhiteTulip", LocalMaterials.WHITE_TULIP));
 	
@@ -53,12 +54,16 @@ public class PlantType
 	 */
 	public static PlantType getPlant(String name, IMaterialReader materialReader) throws InvalidConfigException
 	{
-		PlantType plantType = LOOKUP_MAP.get(name);
+		PlantType plantType = LOOKUP_MAP.get(name.toLowerCase());
 		if (plantType == null)
 		{
-			LocalMaterialData material = materialReader.readMaterial(name);
-			// Fall back on block name + data
-			plantType = new PlantType(material);
+			plantType = ALIAS_LOOKUP_MAP.get(name.toLowerCase());
+			if (plantType == null)
+			{
+				LocalMaterialData material = materialReader.readMaterial(name);
+				// Fall back on block name + data
+				plantType = new PlantType(material);
+			}
 		}
 		return plantType;
 	}
@@ -82,11 +87,19 @@ public class PlantType
 	 */
 	private static PlantType register(PlantType plantType)
 	{
-		LOOKUP_MAP.put(plantType.toString(), plantType);
+		LOOKUP_MAP.put(plantType.toString().toLowerCase(), plantType);
+		if(plantType.aliases != null)
+		{
+			for(String alias : plantType.aliases)
+			{
+				ALIAS_LOOKUP_MAP.put(alias.toLowerCase(), plantType);
+			}
+		}
 		return plantType;
 	}
 
 	private final String name;
+	private final String[] aliases;
 	private LocalMaterialData topBlock;
 	private LocalMaterialData bottomBlock;
 
@@ -96,11 +109,16 @@ public class PlantType
 	 * @param name Custom name for this plant.
 	 * @param material The material of the block.
 	 */
-	private PlantType(String name, LocalMaterialData material)
+	private PlantType(String name, String[] aliases, LocalMaterialData material)
 	{
 		this.name = name;
+		this.aliases = aliases;
 		this.topBlock = null;
 		this.bottomBlock = material;
+	}
+	private PlantType(String name, LocalMaterialData material)
+	{
+		this(name, (String[])null, material);
 	}
 
 	/**
@@ -111,6 +129,7 @@ public class PlantType
 	private PlantType(LocalMaterialData material)
 	{
 		this.name = material.toString();
+		this.aliases = null;
 		this.topBlock = null;
 		this.bottomBlock = material;
 	}
@@ -122,11 +141,16 @@ public class PlantType
 	 * @param bottomMaterial The bottom material of the plant.
 	 * @param topMaterial The top material of the plant, if it has one
 	 */
-	private PlantType(String name, LocalMaterialData bottomMaterial, LocalMaterialData topMaterial)
+	private PlantType(String name, String[] aliases, LocalMaterialData bottomMaterial, LocalMaterialData topMaterial)
 	{
 		this.name = name;
+		this.aliases = aliases;
 		this.bottomBlock = bottomMaterial;
 		this.topBlock = topMaterial;
+	}
+	private PlantType(String name, LocalMaterialData bottomMaterial, LocalMaterialData topMaterial)
+	{
+		this(name, null, bottomMaterial, topMaterial);
 	}
 
 	/**
