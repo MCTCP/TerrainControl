e94ef2630a0260a5a218d450734b8b17883e8d9f
PG85 <rik_lubking@hotmail.com>
2021-08-07 11:08:09 +0200

    1.16.5 - 0.0.18: OceanBiomeSize fix

--- common/common-generator/src/main/java/com/pg85/otg/gen/biome/layers/BiomeLayers.java
+++ common/common-generator/src/main/java/com/pg85/otg/gen/biome/layers/BiomeLayers.java
@@ -98,26 +98,26 @@ public class BiomeLayers
 					factory = new FuzzyScaleLayer().create(contextProvider.apply(2000L), factory);
 				}
 
-				// TODO: worldconfig
 				if (depth == data.oceanBiomeSize && !oceanTemperatureStarted)
 				{
+					// TODO: Process isles/borders for oceanTemperatureFactory too, for gendepths after its been added?
 					oceanTemperatureFactory = new OceanTemperatureLayer(data).create(contextProvider.apply(3L));
 					oceanTemperatureStarted = true;
 				}
-	
+
 				// If the depth is between landSize and landFuzzy, add islands to fuzz the ocean/land border.
 				if (depth < (data.landSize + data.landFuzzy))
 				{
 					factory = new AddIslandsLayer().create(contextProvider.apply(depth), factory);
 				}
-	
+
 				if(data.biomeMode == BiomeMode.Normal)
 				{
 					if (data.groups.containsKey(depth))
 					{
 						factory = new BiomeGroupLayer(data, depth).create(contextProvider.apply(depth), factory);
 					}
-		
+
 					if (data.biomeDepths.contains(depth))
 					{
 						factory = new BiomeLayer(data, depth).create(contextProvider.apply(depth), factory);
@@ -182,7 +182,7 @@ public class BiomeLayers
 						}
 						int chance = (data.biomeRarityScale + 1) - biome.rarity;
 						islesAtCurrentDepth.addIsle(biome.id, chance, biomeCanSpawnIn, inOcean);
-					}	
+					}
 					factory = new BiomeIsleLayer(islesAtCurrentDepth).create(contextProvider.apply(depth), factory);				
 				}
 				
@@ -202,6 +202,7 @@ public class BiomeLayers
 			}
 
 			// Add ocean biomes. This only adds the regular ocean at the moment, soon it will add others.
+			// TODO: This does nothing atm, remove?
 			factory = new ApplyOceanLayer(data).create(contextProvider.apply(3L), factory);
 
 			factory = new MergeOceanTemperatureLayer().create(contextProvider.apply(1L), factory, oceanTemperatureFactory);
@@ -211,6 +212,7 @@ public class BiomeLayers
 			{
 				factory = new FinalizeWithRiverLayer(data.riversEnabled, data.riverBiomes).create(contextProvider.apply(1L), factory, riverFactory);
 			} else {
+				// TODO: This generates no rivers atm
 				factory = new FinalizeLayer(data.riversEnabled, data.riverBiomes).create(contextProvider.apply(1L), factory);
 			}
 		}
--- common/common-generator/src/main/java/com/pg85/otg/gen/biome/layers/OceanTemperatureLayer.java
+++ common/common-generator/src/main/java/com/pg85/otg/gen/biome/layers/OceanTemperatureLayer.java
@@ -7,21 +7,29 @@ public class OceanTemperatureLayer implements InitLayer
 {
 	private final BiomeLayerData data;
 
-	public OceanTemperatureLayer(BiomeLayerData data) {
+	public OceanTemperatureLayer(BiomeLayerData data)
+	{
 		this.data = data;
 	}
 
 	@Override
 	public int sample(LayerSampleContext<?> context, int x, int z)
 	{
-		double noise = context.getNoiseSampler().sample(x / 8.0, z / 8.0, 0, 0, 0);
-		if (noise > 0.4) {
+		double noise = context.getNoiseSampler().sample((double)x / 8.0, (double)z / 8.0D, 0.0D, 0.0D, 0.0D);
+		if (noise > 0.4)
+		{
 			return this.data.oceanTemperatures[0]; // Warm ocean
-		} else if (noise > 0.2) {
+		}
+		else if (noise > 0.2)
+		{
 			return this.data.oceanTemperatures[1]; // Lukewarm ocean
-		} else if (noise < -0.4) {
+		}
+		else if (noise < -0.4)
+		{
 			return this.data.oceanTemperatures[2]; // Frozen ocean
-		} else if (noise < -0.2) {
+		}
+		else if (noise < -0.2)
+		{
 			return this.data.oceanTemperatures[3]; // Cold ocean
 		}
 
