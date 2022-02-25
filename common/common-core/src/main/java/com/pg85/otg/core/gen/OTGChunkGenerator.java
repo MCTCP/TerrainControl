ea3b0d895e351de19fead778827a4c229e39fa45
FrankTCA <frank@steamedveggie.com>
2022-02-25 17:28:58 -0500

    -64 -> -65

--- common/common-core/src/main/java/com/pg85/otg/core/gen/OTGChunkGenerator.java
+++ common/common-core/src/main/java/com/pg85/otg/core/gen/OTGChunkGenerator.java
@@ -636,7 +636,7 @@ public class OTGChunkGenerator implements ISurfaceGeneratorNoiseProvider
 		// TODO: Change this from stone and deepslate noise
 		for (int x = 0; x < 16; x++) {
 			for (int z = 0; z < 16; z++) {
-				for (int y = 0; y > -64; y--) {
+				for (int y = 0; y > -65; y--) {
 					buffer.setBlock(x, y, z, LocalMaterials.STONE);
 				}
 			}
